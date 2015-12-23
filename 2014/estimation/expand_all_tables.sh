bc9026d0b106110577b7cdfa15d9f095c23355c3
Brice Nichols <bnichols@psrc.org>
2015-12-23 11:39:39 -0800

    added some comments :)

--- 2014/estimation/expand_all_tables.sh
+++ 2014/estimation/expand_all_tables.sh
@@ -5,12 +5,14 @@ clear
 echo "Processing all survey tables"
 
 for file in $(ls *.dat); do
-	# echo $file
+	# process all .dat files in this dir
 	input=$file
 
+	# create output name by removing extension and adding tag
 	f=$(echo $file | cut -f 1 -d '.')
 	output=$f'-expanded.csv'
 
+	# call the python script to expand the survey sample dat file
 	echo processing input $input
 	python expand_survey.py $input $output
 	echo output saved as $output
