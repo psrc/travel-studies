19966e2d42c3713a7b78b5bc3db723d44fa6f44d
Michael Jensen <mjensen@psrc.org>
2019-05-06 15:43:36 -0700

    Revisions to 1st Operational version

--- survey_data_cleaning/data2toolsie_prev_next.sql
+++ survey_data_cleaning/data2toolsie_prev_next.sql
@@ -1,27 +1,49 @@
-DROP VIEW IF EXISTS trip_prev_next
+DROP VIEW IF EXISTS trip_pn
 DROP VIEW IF EXISTS data2toolsie_prev_next;
 GO
 
 CREATE VIEW trip_pn
 AS
-(SELECT t1.tripid AS master_tripid, 'current'   AS reference, t1.* FROM trip as t1 JOIN trip_error_flags 	AS tef 	ON t1.personid=tef.personid AND t1.tripnum = tef.tripnum
- UNION ALL
- SELECT t1.tripid AS master_tripid, 'previous'  AS reference, t0.* FROM trip AS t1 JOIN trip 				AS t0 	ON (t1.tripid-1)=t0.tripid
- UNION ALL
- SELECT t1.tripid AS master_tripid, 'next'      AS reference, t2.* FROM trip AS t1 JOIN trip 				AS t2 	ON (t1.tripid+1)=t2.tripid)
+SELECT 
+        tef.tripid AS master_tripid, trip.tripnum, trip.personid,
+        tef.error_flag AS reference,
+        trip.depart_time_timestamp, trip.arrival_time_timestamp, 
+        trip.mode_1, trip.mode_2, trip.mode_3, trip.mode_4,
+        trip.driver, 
+        trip.dest_name, trip.dest_purpose, trip.dest_lat, trip.dest_lng, 
+        trip.trip_path_distance, trip.google_duration, trip.travelers_total, 
+        trip.dest_is_home, trip.dest_is_work
+    FROM trip JOIN trip_error_flags AS tef ON trip.tripid = tef.tripid
+ UNION
+ SELECT 
+        tef.tripid AS master_tripid, trip.tripnum, trip.personid,
+        'previous' AS reference,
+        trip.depart_time_timestamp, trip.arrival_time_timestamp, 
+        trip.mode_1, trip.mode_2, trip.mode_3, trip.mode_4,
+        trip.driver, 
+        trip.dest_name, trip.dest_purpose, trip.dest_lat, trip.dest_lng, 
+        trip.trip_path_distance, trip.google_duration, trip.travelers_total, 
+        trip.dest_is_home, trip.dest_is_work
+    FROM trip JOIN trip_error_flags AS tef ON trip.personid = tef.personid AND trip.tripnum= tef.tripnum-1
+ UNION
+ SELECT 
+        tef.tripid AS master_tripid, trip.tripnum, trip.personid,
+        'next' AS reference,
+        trip.depart_time_timestamp, trip.arrival_time_timestamp, 
+        trip.mode_1, trip.mode_2, trip.mode_3, trip.mode_4,
+        trip.driver, 
+        trip.dest_name, trip.dest_purpose, trip.dest_lat, trip.dest_lng, 
+        trip.trip_path_distance, trip.google_duration, trip.travelers_total, 
+        trip.dest_is_home, trip.dest_is_work
+    FROM trip JOIN trip_error_flags AS tef ON trip.personid = tef.personid AND trip.tripnum= tef.tripnum+1;
 GO
 
 CREATE VIEW data2toolsie_prev_next
 AS
-SELECT  trip_pn.master_tripid, trip_pn.tripnum,
-        trip_pn.reference,
-        trip_pn.personid,age.agedesc, p.worker, p.student,
-        trip_pn.depart_time_timestamp, trip_pn.arrival_time_timestamp, 
-        trip_pn.mode_1, trip_pn.mode_2, trip_pn.mode_3, trip_pn.mode_4, 
-        trip_pn.driver, 
-        trip_pn.dest_name, trip_pn.dest_purpose, trip_pn.dest_lat, trip_pn.dest_lng, 
-        trip_pn.trip_path_distance, trip_pn.google_duration, trip_pn.travelers_total, 
-        trip_pn.dest_is_home, trip_pn.dest_is_work
-FROM trip_pn JOIN person        AS p ON trip_pn.hhid = p.hhid AND trip_pn.personid = p.personid
+SELECT  trip_pn.*, age.agedesc, p.worker, p.student,
+        h.reported_lat AS home_lat, h.reported_lng AS home_lng, 
+		p.work_lat, p.work_lng, 
+		p.school_loc_lat AS school_lat, p.school_loc_lng AS school_lng
+FROM trip_pn JOIN person        AS p ON trip_pn.personid = p.personid
          JOIN hhts_agecodes AS age 	ON p.age = age.agecode
-		 JOIN household 	AS h 	ON trip_pn.hhid = h.hhid;
+		 JOIN household 	AS h 	ON p.hhid = h.hhid;
