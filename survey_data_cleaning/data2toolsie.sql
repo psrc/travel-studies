72318a44a252c0cf3a21f2b7132e9d1ff47d27c2
Michael Jensen <MJensen@psrc.org>
2019-06-26 20:37:47 -0700

    Revisions to frontend view for usability

--- survey_data_cleaning/data2toolsie.sql
+++ survey_data_cleaning/data2toolsie.sql
@@ -15,7 +15,7 @@ GO
 
 DROP VIEW IF EXISTS data2toolsie_t;
 GO
-CREATE VIEW data2toolsie_t
+CREATE VIEW data2toolsie_t 
 AS
 SELECT 	t1.personid, t1.hhid,
 		t1.tripnum, t1.tripid,
@@ -48,10 +48,46 @@ UNION ALL
 SELECT 	t1.personid, t1.hhid,
 		t1.tripnum, t1.tripid, '' AS modes_desc, '' AS depart_time_timestamp,'' AS depart_hhmm, '' AS speed, '' AS hhmemembers,
 		t1.dest_name, tp.purpose AS dest_purpose, 
-		CONCAT(CONVERT(varchar(30), (DATEDIFF(mi, t1.arrival_time_timestamp, t2.depart_time_timestamp) / 60)),'hr',RIGHT('00'+CONVERT(varchar(30), (DATEDIFF(mi, t1.arrival_time_timestamp, t2.depart_time_timestamp) % 60)),2),'min') AS activity_duration_hhmm,
+		CONCAT(CONVERT(varchar(30), (DATEDIFF(mi, t1.arrival_time_timestamp, t2.depart_time_timestamp) / 60)),'hr',RIGHT('00'+CONVERT(varchar(30), (DATEDIFF(mi, t1.arrival_time_timestamp, t2.depart_time_timestamp) % 60)),2),'min') AS duration_at_dest,
 		t1.dest_lat AS lat, t1.dest_lng AS lng, 1 AS dest, 'USA' AS country, 'WA' AS state, t1.dest_city AS city
 		FROM trip AS t1
 			LEFT JOIN trip as t2 ON t1.personid = t2.personid AND (t1.tripnum+1) = t2.tripnum
 			JOIN trip_purpose AS tp ON t1.dest_purpose = tp.purpose_id
 		WHERE t1.hhgroup = 2;
+GO
+
+DROP VIEW IF EXISTS data2frontend;
+GO
+CREATE VIEW data2frontend WITH SCHEMABINDING
+AS
+SELECT t1.personid, t1.hhid,
+		t1.tripnum, 
+        STUFF(	COALESCE(',' + CAST(t1.mode_acc AS nvarchar), '') + 
+				COALESCE(',' + CAST(t1.mode_1 AS nvarchar), '') + 
+				COALESCE(',' + CAST(t1.mode_2 AS nvarchar), '') + 
+				COALESCE(',' + CAST(t1.mode_3 AS nvarchar), '') + 
+				COALESCE(',' + CAST(t1.mode_4 AS nvarchar), '') +
+				COALESCE(',' + CAST(t1.mode_egr AS nvarchar), ''), 1, 1, '') AS modes_desc, 
+		FORMAT(t1.depart_time_timestamp,N'dd- hh\:mm tt','en-US') AS depart_dhm, 
+		ROUND(t1.speed_mph,1) AS mph, 
+		ROUND(t1.trip_path_distance,1) AS miles,
+		FORMAT(t1.arrival_time_timestamp,N'dd- hh\:mm tt','en-US') AS arrive_dhm,
+		CASE WHEN tef.error_flag IS NOT NULL THEN tef.error_flag ELSE '' END AS error,
+		CASE WHEN t1.travelers_total > 1 THEN CONCAT(CAST(t1.travelers_total - 1 AS nvarchar) ,' - ', 
+				STUFF(	COALESCE(',' + CASE WHEN t1.hhmember1 <> t1.personid THEN RIGHT(CAST(t1.hhmember1 AS nvarchar),2) ELSE NULL END, '') +
+				COALESCE(',' + CASE WHEN t1.hhmember2 <> t1.personid THEN RIGHT(CAST(t1.hhmember2 AS nvarchar),2) ELSE NULL END, '') + 
+				COALESCE(',' + CASE WHEN t1.hhmember3 <> t1.personid THEN RIGHT(CAST(t1.hhmember3 AS nvarchar),2) ELSE NULL END, '') + 
+				COALESCE(',' + CASE WHEN t1.hhmember4 <> t1.personid THEN RIGHT(CAST(t1.hhmember4 AS nvarchar),2) ELSE NULL END, '') + 
+				COALESCE(',' + CASE WHEN t1.hhmember5 <> t1.personid THEN RIGHT(CAST(t1.hhmember5 AS nvarchar),2) ELSE NULL END, '') + 
+				COALESCE(',' + CASE WHEN t1.hhmember6 <> t1.personid THEN RIGHT(CAST(t1.hhmember6 AS nvarchar),2) ELSE NULL END, '') + 
+				COALESCE(',' + CASE WHEN t1.hhmember7 <> t1.personid THEN RIGHT(CAST(t1.hhmember7 AS nvarchar),2) ELSE NULL END, '') + 
+				COALESCE(',' + CASE WHEN t1.hhmember8 <> t1.personid THEN RIGHT(CAST(t1.hhmember8 AS nvarchar),2) ELSE NULL END, '') + 
+				COALESCE(',' + CASE WHEN t1.hhmember9 <> t1.personid THEN RIGHT(CAST(t1.hhmember9 AS nvarchar),2) ELSE NULL END, ''), 1, 1, '')) ELSE '' END AS cotravelers,
+			t1.dest_name, tp.purpose AS dest_purpose, 
+			CONCAT(CONVERT(varchar(30), (DATEDIFF(mi, t1.arrival_time_timestamp, t2.depart_time_timestamp) / 60)),'h',RIGHT('00'+CONVERT(varchar(30), (DATEDIFF(mi, t1.arrival_time_timestamp, t2.depart_time_timestamp) % 60)),2),'m') AS activity_duration_hhmm,
+			t1.revision_code AS rc
+	FROM Mike.trip AS t1 LEFT JOIN Mike.trip as t2 ON t1.personid = t2.personid AND (t1.tripnum+1) = t2.tripnum
+	LEFT JOIN Mike.trip_purpose AS tp ON t1.dest_purpose=tp.purpose_id
+	LEFT JOIN Mike.trip_error_flags AS tef ON t1.tripid=tef.tripid
+	WHERE EXISTS (SELECT 1 FROM Mike.trip_error_flags AS tef WHERE t1.personid=tef.personid);
 GO
