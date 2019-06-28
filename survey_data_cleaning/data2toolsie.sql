3a3323f84db35738bcccd0708072af582a749898
Michael Jensen <mjensen@psrc.org>
2019-06-27 17:07:27 -0700

    Revisions to frontend view as requested: split date, mode desc

--- survey_data_cleaning/data2toolsie.sql
+++ survey_data_cleaning/data2toolsie.sql
@@ -58,20 +58,21 @@ GO
 
 DROP VIEW IF EXISTS data2frontend;
 GO
-CREATE VIEW data2frontend WITH SCHEMABINDING
+CREATE VIEW data2frontend
 AS
 SELECT t1.personid, t1.hhid,
 		t1.tripnum, 
-        STUFF(	COALESCE(',' + CAST(t1.mode_acc AS nvarchar), '') + 
-				COALESCE(',' + CAST(t1.mode_1 AS nvarchar), '') + 
-				COALESCE(',' + CAST(t1.mode_2 AS nvarchar), '') + 
-				COALESCE(',' + CAST(t1.mode_3 AS nvarchar), '') + 
-				COALESCE(',' + CAST(t1.mode_4 AS nvarchar), '') +
-				COALESCE(',' + CAST(t1.mode_egr AS nvarchar), ''), 1, 1, '') AS modes_desc, 
-		FORMAT(t1.depart_time_timestamp,N'dd- hh\:mm tt','en-US') AS depart_dhm, 
+        STUFF(	COALESCE(',' + CAST(ma.mode_desc AS nvarchar), '') + 
+				COALESCE(',' + CAST(m1.mode_desc AS nvarchar), '') + 
+				COALESCE(',' + CAST(m2.mode_desc AS nvarchar), '') + 
+				COALESCE(',' + CAST(m3.mode_desc AS nvarchar), '') + 
+				COALESCE(',' + CAST(m4.mode_desc AS nvarchar), '') +
+				COALESCE(',' + CAST(me.mode_desc AS nvarchar), ''), 1, 1, '') AS modes_desc,
+		CONCAT(CAST(t1.daynum AS nvarchar),' ',CASE WHEN DATEDIFF(hh, t1.depart_time_timestamp, t1.arrival_time_timestamp)/24 > .99 THEN CONCAT('+ ',DATEDIFF(hh, t1.depart_time_timestamp, t1.arrival_time_timestamp)/24) ELSE NULL END) AS daynum,	 
+		FORMAT(t1.depart_time_timestamp,N'hh\:mm tt','en-US') AS depart_dhm, 
 		ROUND(t1.speed_mph,1) AS mph, 
 		ROUND(t1.trip_path_distance,1) AS miles,
-		FORMAT(t1.arrival_time_timestamp,N'dd- hh\:mm tt','en-US') AS arrive_dhm,
+		FORMAT(t1.arrival_time_timestamp,N'hh\:mm tt','en-US') AS arrive_dhm,
 		CASE WHEN tef.error_flag IS NOT NULL THEN tef.error_flag ELSE '' END AS error,
 		CASE WHEN t1.travelers_total > 1 THEN CONCAT(CAST(t1.travelers_total - 1 AS nvarchar) ,' - ', 
 				STUFF(	COALESCE(',' + CASE WHEN t1.hhmember1 <> t1.personid THEN RIGHT(CAST(t1.hhmember1 AS nvarchar),2) ELSE NULL END, '') +
@@ -84,10 +85,16 @@ SELECT t1.personid, t1.hhid,
 				COALESCE(',' + CASE WHEN t1.hhmember8 <> t1.personid THEN RIGHT(CAST(t1.hhmember8 AS nvarchar),2) ELSE NULL END, '') + 
 				COALESCE(',' + CASE WHEN t1.hhmember9 <> t1.personid THEN RIGHT(CAST(t1.hhmember9 AS nvarchar),2) ELSE NULL END, ''), 1, 1, '')) ELSE '' END AS cotravelers,
 			t1.dest_name, tp.purpose AS dest_purpose, 
-			CONCAT(CONVERT(varchar(30), (DATEDIFF(mi, t1.arrival_time_timestamp, t2.depart_time_timestamp) / 60)),'h',RIGHT('00'+CONVERT(varchar(30), (DATEDIFF(mi, t1.arrival_time_timestamp, t2.depart_time_timestamp) % 60)),2),'m') AS activity_duration_hhmm,
+			CONCAT(CONVERT(varchar(30), (DATEDIFF(mi, t1.arrival_time_timestamp, t2.depart_time_timestamp) / 60)),'h',RIGHT('00'+CONVERT(varchar(30), (DATEDIFF(mi, t1.arrival_time_timestamp, t2.depart_time_timestamp) % 60)),2),'m') AS duration_at_dest,
 			t1.revision_code AS rc
-	FROM Mike.trip AS t1 LEFT JOIN Mike.trip as t2 ON t1.personid = t2.personid AND (t1.tripnum+1) = t2.tripnum
-	LEFT JOIN Mike.trip_purpose AS tp ON t1.dest_purpose=tp.purpose_id
-	LEFT JOIN Mike.trip_error_flags AS tef ON t1.tripid=tef.tripid
-	WHERE EXISTS (SELECT 1 FROM Mike.trip_error_flags AS tef WHERE t1.personid=tef.personid);
+	FROM trip AS t1 LEFT JOIN trip as t2 ON t1.personid = t2.personid AND (t1.tripnum+1) = t2.tripnum
+	LEFT JOIN trip_mode AS ma ON t1.mode_1=ma.mode_id
+	LEFT JOIN trip_mode AS m1 ON t1.mode_1=m1.mode_id
+	LEFT JOIN trip_mode AS m2 ON t1.mode_2=m2.mode_id
+	LEFT JOIN trip_mode AS m3 ON t1.mode_3=m3.mode_id
+	LEFT JOIN trip_mode AS m4 ON t1.mode_4=m4.mode_id
+	LEFT JOIN trip_mode AS me ON t1.mode_1=me.mode_id
+	LEFT JOIN trip_purpose AS tp ON t1.dest_purpose=tp.purpose_id
+	LEFT JOIN trip_error_flags AS tef ON t1.tripid=tef.tripid
+	WHERE EXISTS (SELECT 1 FROM trip_error_flags AS tef WHERE t1.personid=tef.personid);
 GO
