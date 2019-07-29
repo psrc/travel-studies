a00038e577f4484b6cade477a065dad5f7d7a66a
Michael Jensen <MJensen@psrc.org>
2019-07-29 12:28:21 -0700

    Rename & make specific to FixieUI e.g. key views, drop unused portions

--- survey_data_cleaning/data2toolsie.sql
+++ survey_data_cleaning/data2toolsie.sql
@@ -1,64 +1,6 @@
-DROP VIEW IF EXISTS data2toolsie_hh;
+DROP VIEW IF EXISTS data2fixie;
 GO
-CREATE VIEW data2toolsie_hh
-AS
-SELECT 
-	p.personid, 
-	Concat(CASE WHEN p.worker = 1 THEN 'Wrk ' ELSE '' END, CASE WHEN p.student=2 THEN 'Std PT ' WHEN p.student=3 THEN 'Std FT' ELSE '' END) AS Commute, 
-	ad.age_desc,
-	h.reported_lat AS home_lat, h.reported_lng AS home_lng 
-	FROM person AS p 
-		JOIN age_desc AS ad ON p.age = ad.age_code 
-		JOIN household AS h ON p.hhid = h.hhid
-		JOIN trip AS t1 ON p.hhid = t1.hhid; 
-GO
-
-DROP VIEW IF EXISTS data2toolsie_t;
-GO
-CREATE VIEW data2toolsie_t 
-AS
-SELECT 	t1.personid, t1.hhid,
-		t1.tripnum, t1.recid,
-        STUFF(	COALESCE(',' + tmacc.mode_desc, '') +
-				COALESCE(',' + tm1.mode_desc, '') + 
-				COALESCE(',' + tm2.mode_desc, '') + 
-				COALESCE(',' + tm3.mode_desc, '') + 
-				COALESCE(',' + tm4.mode_desc, '') + 
-				COALESCE(',' + tmegr.mode_desc, ''), 1, 1, '') AS modes_desc, t1.depart_time_timestamp,
-		LEFT(CONVERT(time(0), t1.depart_time_timestamp, 108), 5) AS depart_hhmm, CONCAT(CAST(ROUND(t1.speed_mph,1) AS NVARCHAR(10)),'mph') AS speed, 
-		STUFF(	COALESCE(',' + RIGHT(CAST(t1.hhmember1 AS nvarchar),2), '') +
-				COALESCE(',' + RIGHT(CAST(t1.hhmember2 AS nvarchar),2), '') + 
-				COALESCE(',' + RIGHT(CAST(t1.hhmember3 AS nvarchar),2), '') + 
-				COALESCE(',' + RIGHT(CAST(t1.hhmember4 AS nvarchar),2), '') + 
-				COALESCE(',' + RIGHT(CAST(t1.hhmember5 AS nvarchar),2), '') + 
-				COALESCE(',' + RIGHT(CAST(t1.hhmember6 AS nvarchar),2), '') + 
-				COALESCE(',' + RIGHT(CAST(t1.hhmember7 AS nvarchar),2), '') + 
-				COALESCE(',' + RIGHT(CAST(t1.hhmember8 AS nvarchar),2), '') + 
-				COALESCE(',' + RIGHT(CAST(t1.hhmember9 AS nvarchar),2), ''), 1, 1, '') AS hhmemembers,
-		'' AS dest_name, '' AS dest_purpose, '' AS activity_duration_hhmm, t1.origin_lat AS lat, t1.origin_lng AS lng, 0 as dest, 'USA' AS country, 'WA' AS state, t1.dest_city AS city
-		FROM trip AS t1
-		LEFT JOIN trip_mode AS tmacc ON t1.mode_acc = tmacc.mode_id 
-		LEFT JOIN trip_mode AS tm1 ON t1.mode_1 = tm1.mode_id
-		LEFT JOIN trip_mode AS tm2 ON t1.mode_2 = tm2.mode_id
-		LEFT JOIN trip_mode AS tm3 ON t1.mode_3 = tm3.mode_id
-		LEFT JOIN trip_mode AS tm4 ON t1.mode_4 = tm4.mode_id
-		LEFT JOIN trip_mode AS tmegr ON t1.mode_egr = tmegr.mode_id 
-		WHERE t1.hhgroup = 2 
-UNION ALL
-SELECT 	t1.personid, t1.hhid,
-		t1.tripnum, t1.recid, '' AS modes_desc, '' AS depart_time_timestamp,'' AS depart_hhmm, '' AS speed, '' AS hhmemembers,
-		t1.dest_name, tp.purpose AS dest_purpose, 
-		CONCAT(CONVERT(varchar(30), (DATEDIFF(mi, t1.arrival_time_timestamp, t2.depart_time_timestamp) / 60)),'hr',RIGHT('00'+CONVERT(varchar(30), (DATEDIFF(mi, t1.arrival_time_timestamp, t2.depart_time_timestamp) % 60)),2),'min') AS duration_at_dest,
-		t1.dest_lat AS lat, t1.dest_lng AS lng, 1 AS dest, 'USA' AS country, 'WA' AS state, t1.dest_city AS city
-		FROM trip AS t1
-			LEFT JOIN trip as t2 ON t1.personid = t2.personid AND (t1.tripnum+1) = t2.tripnum
-			JOIN trip_purpose AS tp ON t1.dest_purpose = tp.purpose_id
-		WHERE t1.hhgroup = 2;
-GO
-
-DROP VIEW IF EXISTS data2frontend;
-GO
-CREATE VIEW data2frontend
+CREATE VIEW data2fixie
 AS
 SELECT t1.personid, t1.hhid, t1.pernum, t1.hhgroup, CASE WHEN EXISTS (SELECT 1 FROM trip WHERE trip.psrc_comment IS NOT NULL AND t1.personid = trip.personid) THEN 1 ELSE 0 END AS Elevated, 0 AS Seattle,
 		t1.tripnum, t1.recid, 
@@ -210,3 +152,35 @@ SELECT [recid]
 			,[psrc_comment]
 			,[psrc_resolved]
 FROM trip;
+GO
+
+--create separate views for FixieUI major record divisions
+
+	CREATE VIEW person_rm AS
+	SELECT p.personid, p.hhid, p.pernum, ac.agedesc AS Age, 
+		CASE WHEN p.worker  = 0 THEN 'No' ELSE 'Yes' END AS Works, 
+		CASE WHEN p.student = 1 THEN 'No' WHEN student = 2 THEN 'PT' WHEN p.student = 3 THEN 'FT' ELSE 'No' END AS Studies, 
+		CASE WHEN p.hhgroup = 1 THEN 'rMove' WHEN p.hhgroup = 2 THEN 'rSurvey' ELSE 'n/a' END AS HHGroup
+	FROM HHSurvey.person AS p INNER JOIN MHHSurvey.hhts_agecodes AS ac ON p.age = ac.agecode
+	WHERE Exists (SELECT 1 FROM HHSurvey.trip_error_flags AS tef WHERE tef.personid = p.personid) AND p.hhgroup=1;
+	GO
+
+	CREATE VIEW person_rs AS
+	SELECT p.personid, p.hhid, p.pernum, ac.agedesc AS Age, 
+		CASE WHEN p.worker  = 0 THEN 'No' ELSE 'Yes' END AS Works, 
+		CASE WHEN p.student = 1 THEN 'No' WHEN student = 2 THEN 'PT' WHEN p.student = 3 THEN 'FT' ELSE 'No' END AS Studies, 
+		CASE WHEN p.hhgroup = 1 THEN 'rMove' WHEN p.hhgroup = 2 THEN 'rSurvey' ELSE 'n/a' END AS HHGroup
+	FROM HHSurvey.person AS p INNER JOIN HHSurvey.hhts_agecodes AS ac ON p.age = ac.agecode
+	WHERE Exists (SELECT 1 FROM HHSurvey.trip_error_flags AS tef WHERE tef.personid = p.personid) AND p.hhgroup=2;
+	GO
+
+	CREATE VIEW person_elev AS
+	SELECT p.personid, p.hhid, p.pernum, ac.agedesc AS Age, 
+		CASE WHEN p.worker  = 0 THEN 'No' ELSE 'Yes' END AS Works, 
+		CASE WHEN p.student = 1 THEN 'No' WHEN student = 2 THEN 'PT' WHEN p.student = 3 THEN 'FT' ELSE 'No' END AS Studies, 
+		CASE WHEN p.hhgroup = 1 THEN 'rMove' WHEN p.hhgroup = 2 THEN 'rSurvey' ELSE 'n/a' END AS HHGroup
+	FROM HHSurvey.person AS p INNER JOIN HHSurvey.hhts_agecodes AS ac ON p.age = ac.agecode
+		LEFT JOIN HHSurvey.trip AS t ON p.personid = t.personid AND t.psrc_comment IS NOT NULL
+	WHERE (Exists (SELECT 1 FROM HHSurvey.trip_error_flags AS tef WHERE tef.personid = p.personid) AND t.psrc_comment IS NOT NULL) 
+		OR Exists (SELECT 1 FROM HHSurvey.hh_error_flags AS hef WHERE hef.hhid = p.hhid);
+	GO
