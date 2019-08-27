78890dee5ad245b31e30dafb28abf86cb7bab63b
Michael Jensen <MJensen@psrc.org>
2019-08-27 15:43:24 -0700

    Update to HHSurvey schema and recid field

--- survey_data_cleaning/HHTS_simple_stats.sql
+++ survey_data_cleaning/HHTS_simple_stats.sql
@@ -1,15 +1,15 @@
 --Modes summary
     WITH cte AS
-    (SELECT t.personid, (SELECT MAX(member) FROM (VALUES (t.mode_1), (t.mode_2),(t.mode_3),(t.mode_4)) AS modes(member) WHERE member <> 97) AS mode_x FROM trip AS t)
+    (SELECT t.personid, (SELECT MAX(member) FROM (VALUES (t.mode_1), (t.mode_2),(t.mode_3),(t.mode_4)) AS modes(member) WHERE member <> 97) AS mode_x FROM HHSurvey.Trip AS t)
     SELECT cte.mode_x, count(*) AS tripcount
     FROM cte 
     GROUP BY cte.mode_x ORDER BY cte.mode_x;
 
 --Purposes summary
-    SELECT t.dest_purpose, count(t.tripid) FROM trip AS t GROUP BY t.dest_purpose;
+    SELECT t.dest_purpose, count(t.tripid) FROM HHSurvey.Trip AS t GROUP BY t.dest_purpose;
 
 --Distance category summary
-    WITH cte AS (SELECT round(t.trip_path_distance,0) AS trip_miles FROM trip_nk AS t)
+    WITH cte AS (SELECT round(t.trip_path_distance,0) AS trip_miles FROM HHSurvey.Trip AS t)
     SELECT CASE WHEN cte.trip_miles < 10  THEN ROUND(cte.trip_miles,0)
                 WHEN cte.trip_miles < 100 THEN ROUND(cte.trip_miles,-1)
                 WHEN cte.trip_miles < 1000 THEN ROUND(cte.trip_miles,-2) 
@@ -31,11 +31,11 @@
 --Trip error code count
 SELECT error_flag, [1] AS rMove, [2] AS rSurvey
 FROM
-(SELECT t.hhgroup, tef.error_flag, t.tripid
-FROM trip_error_flags AS tef JOIN trip as t ON t.tripid=tef.tripid) AS SourceTable
+(SELECT t.hhgroup, tef.error_flag, t.recid
+FROM HHSurvey.trip_error_flags AS tef JOIN HHSurvey.Trip as t ON t.recid=tef.recid) AS SourceTable
 PIVOT
 (
- count(tripid)
+ count(recid)
  FOR hhgroup IN ([1], [2])
 ) AS pvt
 ORDER BY pvt.error_flag;
