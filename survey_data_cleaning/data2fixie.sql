c04fbdc481846d3a7f80cdbb9a93d25628865e20
Michael Jensen <MJensen@psrc.org>
2019-10-15 10:59:33 -0700

    Views specific to staff

--- survey_data_cleaning/data2fixie.sql
+++ survey_data_cleaning/data2fixie.sql
@@ -27,7 +27,7 @@ SELECT t1.recid, t1.personid, t1.hhid, t1.pernum, t1.hhgroup, CASE WHEN EXISTS (
 					WHERE tef.recid = t1.recid
 					ORDER BY tef.error_flag DESC
 					FOR XML PATH('')), 1, 1, NULL) AS Error,
-		CASE WHEN t1.travelers_total > 1 THEN CONCAT(CAST(t1.travelers_total - 1 AS nvarchar) ,' - ', 
+		CASE WHEN t1.travelers_total = 1 THEN '' ELSE CONCAT(CAST(t1.travelers_total - 1 AS nvarchar),' - ', 
 				STUFF(	
 					COALESCE(',' + CASE WHEN t1.hhmember1 <> t1.personid AND NOT EXISTS (SELECT flag_value from HHSurvey.NullFlags WHERE flag_value = t1.hhmember1) THEN RIGHT(CAST(t1.hhmember1 AS nvarchar),2) ELSE NULL END, '') +
 					COALESCE(',' + CASE WHEN t1.hhmember2 <> t1.personid AND NOT EXISTS (SELECT flag_value from HHSurvey.NullFlags WHERE flag_value = t1.hhmember2) THEN RIGHT(CAST(t1.hhmember2 AS nvarchar),2) ELSE NULL END, '') + 
@@ -38,7 +38,7 @@ SELECT t1.recid, t1.personid, t1.hhid, t1.pernum, t1.hhgroup, CASE WHEN EXISTS (
 					COALESCE(',' + CASE WHEN t1.hhmember7 <> t1.personid AND NOT EXISTS (SELECT flag_value from HHSurvey.NullFlags WHERE flag_value = t1.hhmember7) THEN RIGHT(CAST(t1.hhmember7 AS nvarchar),2) ELSE NULL END, '') + 
 					COALESCE(',' + CASE WHEN t1.hhmember8 <> t1.personid AND NOT EXISTS (SELECT flag_value from HHSurvey.NullFlags WHERE flag_value = t1.hhmember8) THEN RIGHT(CAST(t1.hhmember8 AS nvarchar),2) ELSE NULL END, '') + 
 					COALESCE(',' + CASE WHEN t1.hhmember9 <> t1.personid AND NOT EXISTS (SELECT flag_value from HHSurvey.NullFlags WHERE flag_value = t1.hhmember9) THEN RIGHT(CAST(t1.hhmember9 AS nvarchar),2) ELSE NULL END, ''), 
-						1, 1, '')) ELSE '' END AS cotravelers,
+						1, 1, '')) END AS cotravelers,
 			CONCAT(t1.o_purpose, '-',tpo.purpose) AS o_purpose, t1.dest_name, CONCAT(t1.d_purpose, '-',tpd.purpose) AS d_purpose, 
 			CONCAT(CONVERT(varchar(30), (DATEDIFF(mi, t1.arrival_time_timestamp, t2.depart_time_timestamp) / 60)),'h',RIGHT('00'+CONVERT(varchar(30), (DATEDIFF(mi, t1.arrival_time_timestamp, CASE WHEN t2.recid IS NULL 
 									 THEN DATETIME2FROMPARTS(DATEPART(year,t1.arrival_time_timestamp),DATEPART(month,t1.arrival_time_timestamp),DATEPART(day,t1.arrival_time_timestamp),3,0,0,0,0) 
@@ -55,7 +55,7 @@ SELECT t1.recid, t1.personid, t1.hhid, t1.pernum, t1.hhgroup, CASE WHEN EXISTS (
 		LEFT JOIN HHSurvey.trip_mode AS me ON t1.mode_egr=me.mode_id
 		LEFT JOIN HHSurvey.trip_purpose AS tpo ON t1.o_purpose=tpo.purpose_id
 		LEFT JOIN HHSurvey.trip_purpose AS tpd ON t1.d_purpose=tpd.purpose_id
-	WHERE EXISTS (SELECT 1 FROM HHSurvey.trip_error_flags WHERE t1.personid=trip_error_flags.personid);
+	WHERE EXISTS (SELECT 1 FROM HHSurvey.trip_error_flags AS tef2 WHERE t1.personid = tef2.personid);
 GO
 
 DROP VIEW IF EXISTS HHSurvey.pass2trip;
@@ -166,9 +166,10 @@ CREATE UNIQUE CLUSTERED INDEX PK_pass2trip ON HHSurvey.pass2trip(recid);
 		CASE WHEN p.student = 1 THEN 'No' WHEN student = 2 THEN 'PT' WHEN p.student = 3 THEN 'FT' ELSE 'No' END AS Studies, 
 		CASE WHEN p.hhgroup = 1 THEN 'rMove' WHEN p.hhgroup = 2 THEN 'rSurvey' ELSE 'n/a' END AS HHGroup
 	FROM HHSurvey.person AS p INNER JOIN HHSurvey.AgeCategories AS ac ON p.age = ac.agecode JOIN HHSurvey.household AS h ON h.hhid = p.hhid
-	WHERE Exists (SELECT 1 FROM HHSurvey.trip_error_flags AS tef WHERE tef.personid = p.personid AND tef.error_flag NOT LIKE 'missing % trip link') AND p.hhgroup=1 AND h.cityofseattle = 1
+	WHERE NOT EXISTS (SELECT 1 FROM HHSurvey.trip_error_flags AS tef WHERE tef.personid = p.personid AND tef.error_flag LIKE 'missing % trip link') AND p.hhgroup=1 AND h.cityofseattle = 1
 		AND NOT EXISTS (SELECT 1 FROM HHSurvey.Trip AS t WHERE p.personid = t.personid AND (t.psrc_comment IS NOT NULL OR t.psrc_resolved IS NOT NULL))
-		AND NOT EXISTS (SELECT 1 FROM HHSurvey.hh_error_flags AS hef WHERE hef.hhid = p.hhid);
+		AND NOT EXISTS (SELECT 1 FROM HHSurvey.hh_error_flags AS hef WHERE hef.hhid = p.hhid)
+		AND Exists (SELECT 1 FROM HHSurvey.trip_error_flags AS tefx WHERE p.personid = tefx.personid);
 	GO
 
 
@@ -180,9 +181,10 @@ CREATE UNIQUE CLUSTERED INDEX PK_pass2trip ON HHSurvey.pass2trip(recid);
 		CASE WHEN p.student = 1 THEN 'No' WHEN student = 2 THEN 'PT' WHEN p.student = 3 THEN 'FT' ELSE 'No' END AS Studies, 
 		CASE WHEN p.hhgroup = 1 THEN 'rMove' WHEN p.hhgroup = 2 THEN 'rSurvey' ELSE 'n/a' END AS HHGroup
 	FROM HHSurvey.person AS p INNER JOIN HHSurvey.AgeCategories AS ac ON p.age = ac.agecode JOIN HHSurvey.household AS h ON h.hhid = p.hhid
-	WHERE Exists (SELECT 1 FROM HHSurvey.trip_error_flags AS tef WHERE tef.personid = p.personid AND tef.error_flag NOT LIKE 'missing % trip link') AND p.hhgroup=1 AND h.cityofseattle <> 1
+	WHERE NOT EXISTS (SELECT 1 FROM HHSurvey.trip_error_flags AS tef WHERE tef.personid = p.personid AND tef.error_flag LIKE 'missing % trip link') AND p.hhgroup=1 AND h.cityofseattle <> 1
 		AND NOT EXISTS (SELECT 1 FROM HHSurvey.Trip AS t WHERE p.personid = t.personid AND (t.psrc_comment IS NOT NULL OR t.psrc_resolved IS NOT NULL))
-		AND NOT EXISTS (SELECT 1 FROM HHSurvey.hh_error_flags AS hef WHERE hef.hhid = p.hhid);
+		AND NOT EXISTS (SELECT 1 FROM HHSurvey.hh_error_flags AS hef WHERE hef.hhid = p.hhid)
+		AND Exists (SELECT 1 FROM HHSurvey.trip_error_flags AS tefx WHERE p.personid = tefx.personid);
 	GO
 
 
@@ -194,9 +196,10 @@ CREATE UNIQUE CLUSTERED INDEX PK_pass2trip ON HHSurvey.pass2trip(recid);
 		CASE WHEN p.student = 1 THEN 'No' WHEN student = 2 THEN 'PT' WHEN p.student = 3 THEN 'FT' ELSE 'No' END AS Studies, 
 		CASE WHEN p.hhgroup = 1 THEN 'rMove' WHEN p.hhgroup = 2 THEN 'rSurvey' ELSE 'n/a' END AS HHGroup
 	FROM HHSurvey.person AS p INNER JOIN HHSurvey.AgeCategories AS ac ON p.age = ac.agecode
-	WHERE Exists (SELECT 1 FROM HHSurvey.trip_error_flags AS tef WHERE tef.personid = p.personid AND tef.error_flag NOT LIKE 'missing % trip link') AND p.hhgroup=2
+	WHERE NOT EXISTS (SELECT 1 FROM HHSurvey.trip_error_flags AS tef WHERE tef.personid = p.personid AND tef.error_flag LIKE 'missing % trip link') AND p.hhgroup=2
 			AND NOT EXISTS (SELECT 1 FROM HHSurvey.Trip AS t WHERE p.personid = t.personid AND (t.psrc_comment IS NOT NULL OR t.psrc_resolved IS NOT NULL))
-			AND NOT EXISTS (SELECT 1 FROM HHSurvey.hh_error_flags AS hef WHERE hef.hhid = p.hhid);
+			AND NOT EXISTS (SELECT 1 FROM HHSurvey.hh_error_flags AS hef WHERE hef.hhid = p.hhid)
+		AND Exists (SELECT 1 FROM HHSurvey.trip_error_flags AS tefx WHERE p.personid = tefx.personid);
 	GO
 
 
@@ -208,10 +211,10 @@ CREATE UNIQUE CLUSTERED INDEX PK_pass2trip ON HHSurvey.pass2trip(recid);
 		CASE WHEN p.student = 1 THEN 'No' WHEN student = 2 THEN 'PT' WHEN p.student = 3 THEN 'FT' ELSE 'No' END AS Studies, 
 		CASE WHEN p.hhgroup = 1 THEN 'rMove' WHEN p.hhgroup = 2 THEN 'rSurvey' ELSE 'n/a' END AS HHGroup
 	FROM HHSurvey.person AS p INNER JOIN HHSurvey.AgeCategories AS ac ON p.age = ac.agecode
-	WHERE Exists (SELECT 1 FROM HHSurvey.Trip AS t WHERE p.personid = t.personid AND t.psrc_comment IS NOT NULL AND t.psrc_resolved IS NULL)
+	WHERE (Exists (SELECT 1 FROM HHSurvey.Trip AS t WHERE p.personid = t.personid AND t.psrc_comment IS NOT NULL AND t.psrc_resolved IS NULL)
 		    OR Exists (SELECT 1 FROM HHSurvey.trip_error_flags AS tef WHERE tef.personid = p.personid AND (tef.error_flag LIKE 'missing % trip link' OR tef.error_flag IN('time overlap')))
-			OR Exists (SELECT 1 FROM HHSurvey.hh_error_flags AS hef WHERE hef.hhid = p.hhid)
-			AND Exists (SELECT 1 FROM HHSurvey.Trip AS t WHERE p.personid = t.personid);
+			OR Exists (SELECT 1 FROM HHSurvey.hh_error_flags AS hef WHERE hef.hhid = p.hhid))
+		AND Exists (SELECT 1 FROM HHSurvey.trip_error_flags AS tefx WHERE p.personid = tefx.personid);
 	GO
 
 	DROP VIEW IF EXISTS HHSurvey.person_by_error;
@@ -223,6 +226,7 @@ CREATE UNIQUE CLUSTERED INDEX PK_pass2trip ON HHSurvey.pass2trip(recid);
 		CASE WHEN p.hhgroup = 1 THEN 'rMove' WHEN p.hhgroup = 2 THEN 'rSurvey' ELSE 'n/a' END AS HHGroup
 	FROM HHSurvey.person AS p INNER JOIN HHSurvey.AgeCategories AS ac ON p.age = ac.agecode
 	WHERE Exists (SELECT 1 FROM HHSurvey.trip_error_flags AS tef JOIN HHSurvey.Trip AS t ON tef.recid = t.recid WHERE tef.personid = p.personid AND tef.error_flag IN('mode_1 missing','too slow','purpose missing') AND t.psrc_comment IS NULL AND t.psrc_resolved IS NULL)
+		AND Exists (SELECT 1 FROM HHSurvey.trip_error_flags AS tefx WHERE p.personid = tefx.personid);
 	--		AND NOT EXISTS (SELECT 1 FROM HHSurvey.trip_error_flags AS tef WHERE tef.personid = p.personid AND tef.error_flag LIKE 'missing % trip link' );
 	GO
 
@@ -328,13 +332,44 @@ CREATE UNIQUE CLUSTERED INDEX PK_pass2trip ON HHSurvey.pass2trip(recid);
 	END
 	GO
 
+	DROP PROCEDURE IF EXISTS HHSurvey.examine_link_ingredients;
+	GO
+	CREATE PROCEDURE HHSurvey.examine_link_ingredients
+		@target_recid numeric NULL --provide recid of reference member
+	
+	AS BEGIN
+	SET NOCOUNT OFF;
+	WITH cte AS
+	(SELECT tid0.personid, tid0.trip_link FROM HHSurvey.trip_ingredients_done AS tid0 WHERE tid0.recid = @target_recid)
+	SELECT tid.recid, tid.personid, tid.hhid, tid.tripnum, tid.daynum, tid.mode_1,
+		FORMAT(tid.depart_time_timestamp,N'hh\:mm tt','en-US') AS depart_dhm,
+		FORMAT(tid.arrival_time_timestamp,N'hh\:mm tt','en-US') AS arrive_dhm,
+		ROUND(tid.trip_path_distance,1) AS miles,
+		ROUND(tid.speed_mph,1) AS mph, 
+		CONCAT(tid.o_purpose, '-',tpo.purpose) AS o_purpose, tid.dest_name, CONCAT(tid.d_purpose, '-',tpd.purpose) AS d_purpose, 
+			CONCAT(CONVERT(varchar(30), (DATEDIFF(mi, tid.arrival_time_timestamp, t2.depart_time_timestamp) / 60)),'h',RIGHT('00'+CONVERT(varchar(30), (DATEDIFF(mi, tid.arrival_time_timestamp, CASE WHEN t2.recid IS NULL 
+									 THEN DATETIME2FROMPARTS(DATEPART(year,tid.arrival_time_timestamp),DATEPART(month,tid.arrival_time_timestamp),DATEPART(day,tid.arrival_time_timestamp),3,0,0,0,0) 
+									 ELSE t2.depart_time_timestamp END) % 60)),2),'m') AS duration_at_dest,
+			CONCAT(CAST(tid.origin_lat AS VARCHAR(20)),', ',CAST(tid.origin_lng AS VARCHAR(20))) AS origin_coord,						 
+			CONCAT(CAST(tid.dest_lat AS VARCHAR(20)),', ',CAST(tid.dest_lng AS VARCHAR(20))) AS dest_coord,
+			tid.revision_code AS rc, tid.psrc_comment AS elevate_issue
+		FROM HHSurvey.trip_ingredients_done AS tid 
+			JOIN cte ON tid.trip_link = cte.trip_link AND tid.personid = cte.personid
+			LEFT JOIN HHSurvey.trip_ingredients_done as t2 ON tid.personid = t2.personid AND (tid.tripnum+1) = t2.tripnum
+			LEFT JOIN HHSurvey.trip_purpose AS tpo ON tid.o_purpose=tpo.purpose_id
+			LEFT JOIN HHSurvey.trip_purpose AS tpd ON tid.d_purpose=tpd.purpose_id
+	ORDER BY tid.tripnum ASC;
+	END
+	GO
+
 	DROP PROCEDURE IF EXISTS HHSurvey.link_trip_click;
 	GO
 	CREATE PROCEDURE HHSurvey.link_trip_click
-		@ref_recid int = NULL,
-		@recid_list nvarchar(255) NULL
+		@ref_recid int = NULL
+	
 		AS BEGIN
 	SET NOCOUNT OFF;
+	DECLARE @recid_list nvarchar(255) = NULL
 	IF (SELECT HHSurvey.RgxFind(HHSurvey.TRIM(t.psrc_comment),'^(\d+,?)+$',1) FROM HHSurvey.Trip AS t WHERE t.recid = @ref_recid) = 1
 		BEGIN
 		SELECT @recid_list = (SELECT HHSurvey.TRIM(t.psrc_comment) FROM HHSurvey.Trip AS t WHERE t.recid = @ref_recid)
@@ -342,4 +377,58 @@ CREATE UNIQUE CLUSTERED INDEX PK_pass2trip ON HHSurvey.pass2trip(recid);
 		SELECT @recid_list = NULL, @ref_recid = NULL
 		END
 	END
+	GO
+	
+	DROP VIEW IF EXISTS HHSurvey.Person_Mike
+	DROP VIEW IF EXISTS HHSurvey.Person_Neil
+	DROP VIEW IF EXISTS HHSurvey.person_Grant
+	DROP VIEW IF EXISTS HHSurvey.person_Alex
+	GO
+
+	CREATE VIEW HHSurvey.person_Alex WITH SCHEMABINDING AS
+	SELECT p.personid, p.hhid AS hhid, p.pernum, ac.agedesc AS Age, 
+		CASE WHEN p.worker  = 0 THEN 'No' ELSE 'Yes' END AS Works, 
+		CASE WHEN p.student = 1 THEN 'No' WHEN student = 2 THEN 'PT' WHEN p.student = 3 THEN 'FT' ELSE 'No' END AS Studies, 
+		CASE WHEN p.hhgroup = 1 THEN 'rMove' WHEN p.hhgroup = 2 THEN 'rSurvey' ELSE 'n/a' END AS HHGroup
+	FROM HHSurvey.person AS p INNER JOIN HHSurvey.AgeCategories AS ac ON p.age = ac.agecode
+	WHERE Exists (SELECT 1 FROM HHSurvey.trip_error_flags AS tefx WHERE p.personid = tefx.personid AND tefx.error_flag IN('purpose missing'));
+	GO
+
+	CREATE VIEW HHSurvey.person_Grant WITH SCHEMABINDING AS
+	SELECT p.personid, p.hhid AS hhid, p.pernum, ac.agedesc AS Age, 
+		CASE WHEN p.worker  = 0 THEN 'No' ELSE 'Yes' END AS Works, 
+		CASE WHEN p.student = 1 THEN 'No' WHEN student = 2 THEN 'PT' WHEN p.student = 3 THEN 'FT' ELSE 'No' END AS Studies, 
+		CASE WHEN p.hhgroup = 1 THEN 'rMove' WHEN p.hhgroup = 2 THEN 'rSurvey' ELSE 'n/a' END AS HHGroup
+	FROM HHSurvey.person AS p INNER JOIN HHSurvey.AgeCategories AS ac ON p.age = ac.agecode
+	WHERE NOT EXISTS (SELECT 1 FROM HHSurvey.trip_error_flags AS tef WHERE tef.personid = p.personid AND tef.error_flag LIKE 'missing % trip link' OR tef.error_flag IN('excessive speed','too slow'))
+			AND NOT EXISTS (SELECT 1 FROM HHSurvey.Trip AS t WHERE p.personid = t.personid AND (t.psrc_comment IS NOT NULL OR t.psrc_resolved IS NOT NULL))
+			AND NOT EXISTS (SELECT 1 FROM HHSurvey.hh_error_flags AS hef WHERE hef.hhid = p.hhid)
+			AND EXISTS (SELECT 1 FROM HHSurvey.trip_error_flags AS tefx WHERE p.personid = tefx.personid)
+		AND NOT EXISTS (SELECT 1 FROM HHSurvey.person_Alex AS pa WHERE pa.personid = p.personid);
+	GO
+
+	CREATE VIEW HHSurvey.person_Neil WITH SCHEMABINDING AS
+	SELECT p.personid, p.hhid AS hhid, p.pernum, ac.agedesc AS Age, 
+		CASE WHEN p.worker  = 0 THEN 'No' ELSE 'Yes' END AS Works, 
+		CASE WHEN p.student = 1 THEN 'No' WHEN student = 2 THEN 'PT' WHEN p.student = 3 THEN 'FT' ELSE 'No' END AS Studies, 
+		CASE WHEN p.hhgroup = 1 THEN 'rMove' WHEN p.hhgroup = 2 THEN 'rSurvey' ELSE 'n/a' END AS HHGroup
+	FROM HHSurvey.person AS p INNER JOIN HHSurvey.AgeCategories AS ac ON p.age = ac.agecode
+	WHERE (Exists (SELECT 1 FROM HHSurvey.Trip AS t WHERE p.personid = t.personid AND t.psrc_comment IS NOT NULL AND t.psrc_resolved IS NULL)
+		    OR Exists (SELECT 1 FROM HHSurvey.trip_error_flags AS tef WHERE tef.personid = p.personid AND (tef.error_flag LIKE 'missing % trip link' OR tef.error_flag IN('missing next trip link','too long at dest','same dest as next','underage driver','unlicensed driver')))
+			OR Exists (SELECT 1 FROM HHSurvey.hh_error_flags AS hef WHERE hef.hhid = p.hhid))
+		AND Exists (SELECT 1 FROM HHSurvey.trip_error_flags AS tefx WHERE p.personid = tefx.personid)
+		AND NOT EXISTS (SELECT 1 FROM HHSurvey.person_Alex AS pa WHERE pa.personid = p.personid) 
+		AND NOT EXISTS (SELECT 1 FROM HHSurvey.person_Grant AS pg WHERE pg.personid = p.personid);
+	GO
+
+	CREATE VIEW HHSurvey.person_Mike WITH SCHEMABINDING AS
+	SELECT p.personid, p.hhid AS hhid, p.pernum, ac.agedesc AS Age, 
+		CASE WHEN p.worker  = 0 THEN 'No' ELSE 'Yes' END AS Works, 
+		CASE WHEN p.student = 1 THEN 'No' WHEN student = 2 THEN 'PT' WHEN p.student = 3 THEN 'FT' ELSE 'No' END AS Studies, 
+		CASE WHEN p.hhgroup = 1 THEN 'rMove' WHEN p.hhgroup = 2 THEN 'rSurvey' ELSE 'n/a' END AS HHGroup
+	FROM HHSurvey.person AS p INNER JOIN HHSurvey.AgeCategories AS ac ON p.age = ac.agecode
+	WHERE Exists (SELECT 1 FROM HHSurvey.trip_error_flags AS tefx WHERE p.personid = tefx.personid)
+		AND NOT EXISTS (SELECT 1 FROM HHSurvey.person_Alex AS pa WHERE pa.personid = p.personid) 
+		AND NOT EXISTS (SELECT 1 FROM HHSurvey.person_Grant AS pg WHERE pg.personid = p.personid)
+		AND NOT EXISTS (SELECT 1 FROM HHSurvey.person_Neil AS pn WHERE pn.personid = p.personid);
 	GO
