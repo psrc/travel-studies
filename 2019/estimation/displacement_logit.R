d0ec787594aac2a92e1e425a2d5e249588375c2c
Suzanne Childress <childresssuzanne@gmail.com>
2020-05-27 12:01:44 -0700

    More work on displacement model

--- 2019/estimation/displacement_logit.R
+++ 2019/estimation/displacement_logit.R
@@ -7,8 +7,12 @@ library(odbc)
 library(DBI)
 library(fastDummies)
 library(aod)
+library(BMA)
 
-missing_codes <- c('Missing: Technical Error', 'Missing: Non-response', 'Missing: Skip logic', 'Children or missing')
+displ_index_data<- 'C:/Users/SChildress/Documents/HHSurvey/displace_estimate/displacement_risk_estimation.csv'
+
+missing_codes <- c('Missing: Technical Error', 'Missing: Non-response', 
+                   'Missing: Skip logic', 'Children or missing')
 
 ## Read from Elmer
 
@@ -33,15 +37,17 @@ read.dt <- function(astring, type =c('table_name', 'sqlquery')) {
 }
 
 
-missing_codes <- c('Missing: Technical Error', 'Missing: Non-response', 'Missing: Skip logic', 'Children or missing')
-dbtable.person.query<- paste("SELECT *  FROM HHSurvey.v_persons_2017_2019")
+dbtable.person.query<- paste("SELECT *  FROM HHSurvey.v_persons_2017_2019_displace_estimation")
 hh_weight_name <- 'hh_wt_combined'
 
+displ_risk_df <- read.csv(displ_index_data)
 
 person_dt<-read.dt(dbtable.person.query, 'tablename')
 
 res_factors<-c("prev_res_factors_forced", "prev_res_factors_housing_cost","prev_res_factors_income_change",
-               "prev_res_factors_community_change")
+               "prev_res_factors_community_change", "prev_home_wa")
+
+
 
 person_dt<-drop_na(person_dt, res_factors)
 # remove missing data
@@ -61,29 +67,77 @@ for (factor in res_factors){
 }
 
 
-# dfs better for making dummy variables
+#prev_home_taz_2010
+person_df$census_2010_tract <- as.character(person_df$census_2010_tract)
+person_df$displa <- as.character(person_df$census_2010_tract)
+
+person_df_dis <- merge(person_df,displ_risk_df, by.x='census_2010_tract', by.y='GEOID', all.x=TRUE)
 
 
-person_df_dum <- dummy_cols(person_df)
+vars_to_consider <- c('displaced', "white","poor_english","no_bachelors","rent","cost_burdened", 
+                      "severe_cost_burdened","poverty_200"	,
+                      "ln_jobs_auto_30", "ln_jobs_transit_45", "transit_qt_mile","transit_2025_half",
+                       "dist_super", "dist_pharm", "dist_rest","dist_park",	"dist_school",
+                      "prox_high_inc",	"at_risk_du","voting",
+                      'displaced', "hhincome_broad", 'race_category', 'education', 
+                      'age_category', 'numchildren', 'numadults', 'numworkers','lifecycle','prev_rent_own',
+                      'prev_res_type','hhincome_detailed','res_dur', 'hhsize',
+                      'vehicle_count', 'student', 'license')
 
-vars_to_consider <- c('displaced', "hhincome_broad_Under $25,000", "hhincome_broad_$25,000-$49,999","res_type_Single-family house (detached house)","race_category_African-American, Hispanic, Multiracial, and Other",
-                      "seattle_home_Home in Seattle",)
-person_df_dum_short<-person_df_dum[vars_to_consider]
-summary(person_df_dum_short)
+person_df_dis_sm <-person_df_dis[vars_to_consider]
+dummy_col_names<-c("hhincome_broad", 'race_category', 'education', 
+                   'age_category', 'numchildren', 'numadults', 'numworkers','lifecycle','prev_rent_own',
+                   'prev_res_type','hhincome_detailed','res_dur', 'hhsize',
+                   'vehicle_count', 'student', 'license')
+person_df_dum <- dummy_cols(person_df_dis_sm, select_columns=dummy_col_names)
 
-#writeClipboard(names(person_df_dum))
-writeClipboard(summary(person_df_dum))
-displ_logit<- glm(displaced ~ `hhincome_broad_Under $25,000`+`hhincome_broad_$25,000-$49,999`+`res_type_Single-family house (detached house)`
-                  +`vehicle_count_0 (no vehicles)`
-                  +`hhincome_broad_$50,000-$74,999`
-                  +`rent_own_Rent`
-                  +`lifecycle_Household includes children under 5`
-                  +`lifecycle_Household size = 1, Householder age 65+`
-                  +`gender_Female`
-                  +`employment_Not currently employed`
-                  +`commute_mode_Bus (public transit)`
-                  +`race_category_African-American, Hispanic, Multiracial, and Other`
-                  +`race_category_Asian Only`
-                  +`sample_county_King`
-              ,data = person_df_dum, family = "binomial")
-summary(displ_logit)
+
+
+less_vars<-c("`hhincome_detailed_Under $10,000`",
+             "`hhincome_detailed_$10,000-$24,999`",
+             "`hhincome_detailed_$25,000-$34,999`",                              
+            "`hhincome_detailed_$35,000-$49,999`",                              
+             "`hhincome_detailed_$50,000-$74,999`",                               
+            "`hhincome_detailed_$75,000-$99,999`" ,                              
+            "`hhincome_detailed_Prefer not to answer`",
+             "transit_qt_mile" ,                                                
+             "transit_2025_half",                                             
+             "dist_super",                                                     
+            "dist_pharm",                                                     
+            "dist_rest",                                                       
+            "dist_park",                                                      
+             "dist_school")
+            
+            
+            
+#             "`race_category_African-American, Hispanic, Multiracial, and Other`",
+#             "`race_category_Asian Only`" ,
+#             "`ln_jobs_auto_30`",
+#             "`ln_jobs_transit_45`"           
+# )
+ 
+ person_df_dum_ls<-person_df_dum[less_vars]
+ 
+ displ_logit<-glm(reformulate(less_vars,'displaced'), data=person_df_dum,
+                           family = 'binomial')
+ summary(displ_logit)
+
+ 
+ 
+ # playing with the estimation manually
+ # displ_logit<- glm(displaced ~ `hhincome_detailed_Under $10,000` +`hhincome_detailed_$10,000-$24,999`     
+ #                   + `hhincome_detailed_$25,000-$34,999`+`hhincome_detailed_$50,000-$74,999`+
+ #                     `hhincome_detailed_$75,000-$99,999`
+ #                   +`race_category_African-American, Hispanic, Multiracial, and Other`
+ #                   +`race_category_Asian Only`
+ #                   ,data = person_df_dum, family = "binomial")
+ # summary(displ_logit)
+                              
+ # using the bma library to select variables
+ x<-person_df_dum[, !names(person_df_dum) %in% c('displaced')]
+ y<-person_df_dum$displaced
+
+ glm.out <- bic.glm(x, y, strict = FALSE, OR = 20,
+                       glm.family="binomial", factor.type=TRUE)
+ summary(glm.out)
+ imageplot.bma(glm.out)
