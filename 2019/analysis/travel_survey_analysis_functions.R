1338611c5077becfc7220e5cf0e54dda50c0c72a
Polina Butrina <40416741+polinabutrina@users.noreply.github.com>
2020-09-23 17:40:10 -0700

    Update travel_survey_analysis_functions.R

--- 2019/analysis/travel_survey_analysis_functions.R
+++ 2019/analysis/travel_survey_analysis_functions.R
@@ -7,6 +7,7 @@ library(DT)
 library(openxlsx)
 library(odbc)
 library(DBI)
+library(psych)
 
 
 # Functions ----------------------------------------------------------------
@@ -17,6 +18,8 @@ library(DBI)
 # Statistical assumptions for margins of error
 p_MOE <- 0.5
 z<-1.645
+missing_codes <- c('Missing: Technical Error', 'Missing: Non-response', 
+                   'Missing: Skip logic', 'Children or missing', ' Prefer not to answer')
 
 # connecting to Elmer
 db.connect <- function() {
@@ -38,6 +41,31 @@ read.dt <- function(astring, type =c('table_name', 'sqlquery')) {
   }
   dbDisconnect(elmer_connection)
   dtelm
+}
+
+
+#Create a crosstab from one variable, calculate counts, totals, and shares,
+# for categorical data
+create_table_one_var = function(var1, table_temp,table_type ) {
+  #table_temp = recategorize_var_upd(var2,table_temp)
+  #print(table_temp)
+  if (table_type == "household" | table_type == "person" ) {
+    weight_2017 = "hh_wt_revised"
+    weight_2019 = "hh_wt_2019"
+    weight_comb = "hh_wt_combined"
+  } else if (table_type == "trip") {
+    weight_2017 = "trip_weight_revised"
+    weight_2019 = "trip_wt_2019"
+    weight_comb = "trip_wt_combined"  
+  } 
+  
+  temp = table_temp %>% select(!!sym(var1), all_of(weight_2017), all_of(weight_2019), all_of(weight_comb)) %>% 
+    filter(!.[[1]] %in% missing_codes, !is.na(.[[1]])) %>% 
+    group_by(!!sym(var1)) %>% 
+    summarise(n=n(),sum_wt_comb = sum(.data[[weight_comb]],na.rm = TRUE),sum_wt_2017 = sum(.data[[weight_2017]],na.rm = TRUE),sum_wt_2019 = sum(.data[[weight_2019]],na.rm = TRUE)) %>% 
+    mutate(perc_comb = sum_wt_comb/sum(sum_wt_comb)*100, perc_2017 = sum_wt_2017/sum(sum_wt_2017)*100, perc_2019 = sum_wt_2019/sum(sum_wt_2019)*100,delta = perc_2019-perc_2017) %>% 
+    ungroup() %>%  mutate(MOE=1.65*(0.25/sum(n))^(1/2)*100) %>% arrange(desc(perc_comb))
+  return(temp)
 }
 
 #Create a crosstab from two variables, calculate counts, totals, and shares,
@@ -90,3 +118,50 @@ write_cross_tab<-function(out_table, var1, var2, file_loc){
 # and then pass this variable to the read.dt function
 sql.query = paste("SELECT * FROM HHSurvey.v_persons_2017_2019_in_house")
 person = read.dt(sql.query, 'sqlquery')
+
+#If you would like to use the table name, instead of query, you can use the following code
+#that will produce the same results
+person = read.dt("HHSurvey.v_persons_2017_2019", 'table_name')
+
+
+#Check the data
+# this step will allow you to understand the variable and the table that you are analyzing
+#you can use the following functions to check for missing values, categories, etc.
+
+#this function will allow you to see all of the variables in the table, check the data type,
+#and see the first couple of values for each of the variables
+glimpse(person)
+
+# to check the distribution of a specific variable, you can use the following code
+#here, for example, we are looking at mode_freq_5 category 
+person %>% group_by() %>% summarise(n=n())
+
+#if you analyze a numerical variable, you can use the following code to see the variable range
+describe()
+
+
+#to delete NA you can use the following code
+#the best practices suggest to create a new variable with the updated table
+
+person_no_na = person %>% filter(!is.na(mode_freq_5))
+
+#when we re-run the distribution code, we see that we've eliminated NAs
+person_no_na %>% group_by(mode_freq_5) %>% summarise(n=n())
+
+# to exclude missing codes, you can use the following code
+#note, that we've assigned missing_codes at the beginning of the script(lines 20-21)
+person_no_na = person_no_na %>% filter(!mode_freq_5 %in% missing_codes)
+
+
+#Create summaries
+
+# to create a summary table based on one variable, you can use create_table_one_var function.
+#The function create_table_one_var has 3 arguments:
+# first, you have to specify the variable you are analyzing
+#second, enter table name
+#third, specify the table type e.g. person, household, vehicle, trip, or day. This will
+# help to use correct weights
+
+#here is an example for mode_freq_5 variable
+
+create_table_one_var("mode_freq_5", person_no_na,"person" )
