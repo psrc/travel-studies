19b99fe306d9ee76a6865cd170d0ec20ec3f56bd
Michael Jensen <mjensen@psrc.org>
2021-08-20 12:10:48 -0700

    Return home revised to use API

--- survey_data_cleaning/fixie_sprocs.sql
+++ survey_data_cleaning/fixie_sprocs.sql
@@ -445,32 +445,71 @@ GO
 
 		CREATE PROCEDURE HHSurvey.insert_return_home
 		AS BEGIN
-		DROP TABLE IF EXISTS HHSurvey.ApiHomeTravel;
+		DROP TABLE IF EXISTS tmpApi2Home;
+		CREATE TABLE tmpApi2Home(rownum int identity(1,1),
+								 init_recid int,
+								 new_recid int,
+								 hhid int, 
+								 personid decimal(19,0),
+								 pernum int,
+								 hhgroup int,
+								 [data_source] int,
+								 api_response nvarchar(255), 
+								 depart_time_timestamp datetime2,
+								 api_minutes float, 
+								 origin_geog geography, 
+							     home_geog geography, 
+								 mode_1 int, 
+								 travelers_hh int, 
+								 travelers_nonhh int, 
+								 travelers_total int, 
+								 api_miles float);
 
-		SELECT t.recid, h.home_lat, h.home_lng, t.dest_lat AS origin_lat, t.dest_lng AS origin_lng, 
-			CAST(Elmer.dbo.rgx_replace(t.psrc_comment,'ADD RETURN HOME( \d?\d):\d\d.*',LTRIM('$1'),1) AS int) AS hh,
-			CAST(RIGHT(Elmer.dbo.rgx_extract(t.psrc_comment,':\d\d',1),2) AS int) AS mm,
-			CAST(0 AS decimal(8,4)) AS travel_duration, CAST(0 AS decimal(8,4)) AS travel_distance --HERE NEED API steps to return travel time and distance
-		INTO HHSurvey.ApiHomeTravel
-		FROM HHSurvey.Trip AS t JOIN HHSurvey.Household AS h ON t.hhid = h.hhid WHERE Elmer.dbo.rgx_find(t.psrc_comment,'ADD RETURN HOME \d?\d:\d\d',1) = 1;
+		
+		INSERT INTO tmpApi2Home(init_recid, hhid, personid, pernum, hhgroup, [data_source], api_response, mode_1, depart_time_timestamp, origin_geog, home_geog, travelers_hh, travelers_nonhh, travelers_total)
+		SELECT t.recid AS init_recid, t.hhid, t.personid, t.pernum, t.hhgroup, t.[data_source],
+			   Elmer.dbo.route_mi_min(t.dest_geog.Long, t.dest_geog.Lat, h.home_geog.Long, h.home_geog.Lat, 
+			   						  CASE WHEN p.age <5 AND t.dest_purpose=6 AND t.dest_geog.STDistance(h.home_geog) < 1500 THEN 'walking' ELSE 'driving' END,'[INSERT BING KEY HERE]') AS api_response,
+			   CASE WHEN p.age <5 AND t.dest_purpose=6 AND t.dest_geog.STDistance(h.home_geog) < 1500 THEN 1 ELSE 16 END	AS mode_1, 
+			   DATETIME2FROMPARTS(YEAR(t.arrival_time_timestamp), 
+			   					  MONTH(t.arrival_time_timestamp), 
+								  DAY(t.arrival_time_timestamp), 
+								  CAST(Elmer.dbo.rgx_replace(t.psrc_comment,'ADD RETURN HOME( \d?\d):\d\d.*',LTRIM('$1'),1) AS int), 
+								  CAST(RIGHT(Elmer.dbo.rgx_extract(t.psrc_comment,':\d\d',1),2) AS int), 0 ,0 ,0) AS depart_time_timestamp,
+			   t.dest_geog,
+			   h.home_geog,
+			   t.travelers_hh, 
+			   t.travelers_nonhh,
+			   t.travelers_total
+			FROM HHSurvey.Trip AS t JOIN HHSurvey.Household AS h ON t.hhid=h.hhid JOIN HHSurvey.Person AS p ON t.personid=p.personid
+			WHERE t.psrc_comment LIKE 'ADD RETURN HOME%';
 
-		INSERT INTO	HHSurvey.Trip (hhid, personid, pernum, hhgroup, data_source, psrc_inserted,
-				dest_lat, dest_lng, dest_name, origin_lat, origin_lng, depart_time_timestamp, arrival_time_timestamp, travel_time, trip_path_distance,
+		WITH cte AS (SELECT max(recid) AS max_recid FROM HHSurvey.Trip)	
+		UPDATE ta
+			SET new_recid = (cte.max_recid + ta.rownum),
+				api_miles = CAST(Elmer.dbo.rgx_replace(api_result,'^(.*),.*','$1',1) AS float), 
+		        api_minutes = CAST(Elmer.dbo.rgx_replace(api_result,'.*,(.*)$','$1',1) AS float)
+			FROM tmpApi2Home AS ta JOIN cte ON 1=1;		
+
+		WITH cte AS (SELECT max(recid) AS max_recid FROM HHSurvey.Trip)
+		INSERT INTO	HHSurvey.Trip (recid, hhid, personid, pernum, hhgroup, [data_source], psrc_inserted,
+				dest_lat, dest_lng, dest_name, origin_lat, origin_lng, depart_time_timestamp, arrival_time_timestamp, trip_path_distance,
 				dest_purpose, mode_1, travelers_hh, travelers_nonhh, travelers_total)
-		SELECT t.hhid, t.personid, t.pernum, t.hhgroup, t.data_source, 1,
-				aht.home_lat, aht.home_lng, 'HOME', aht.origin_lat, aht.origin_lng, 
-				DATETIME2FROMPARTS(YEAR(t.arrival_time_timestamp), MONTH(t.arrival_time_timestamp), DAY(t.arrival_time_timestamp), aht.hh, aht.mm, 0 ,0 ,0) AS depart_time_timestamp, 
-				DATEADD(Minute, ROUND(aht.travel_duration,0), DATETIME2FROMPARTS(YEAR(t.arrival_time_timestamp), MONTH(t.arrival_time_timestamp), DAY(t.arrival_time_timestamp), aht.hh, aht.mm, 0 ,0 ,0)) AS arrival_time_timestamp,
-				aht.travel_duration, aht.travel_distance,
-				1 AS dest_purpose, 16 AS mode_1, t.travelers_hh, t.travelers_nonhh, t.travelers_total
-			FROM HHSurvey.Trip AS t JOIN HHSurvey.ApiHomeTravel AS aht ON t.recid = aht.recid;
+		SELECT  ta.new_recid AS recid, ta.hhid, ta.personid, ta.pernum, ta.hhgroup, ta.[data_source], 1,
+				ta.home_geog.Lat, ta.home_geog.Long, 'HOME', ta.origin_geog.Lat, ta.origin_geog.Long, depart_time_timestamp, 
+				DATEADD(Minute, ROUND(ta.api_minutes,0), ta.depart_time_timestamp) AS arrival_time_timestamp,
+				ta.api_miles, 1 AS dest_purpose, ta.mode_1, ta.travelers_hh, ta.travelers_nonhh, ta.travelers_total
+			FROM tmpApi2Home AS ta;
 
 		UPDATE t 
 			SET t.psrc_comment = Elmer.dbo.rgx_replace(t.psrc_comment, 'ADD RETURN HOME \d?\d:\d\d','',1) 
-			FROM HHSurvey.Trip AS t JOIN HHSurvey.ApiHomeTravel AS aht ON t.recid = aht.recid;
+			FROM HHSurvey.Trip AS t JOIN tmpApi2Home AS ta ON t.recid = ta.init_recid;
 		UPDATE nxt
-		 	SET nxt.origin_purpose = 1, nxt.origin_lat = aht.home_lat, nxt.origin_lng = aht.home_lng
-			FROM HHSurvey.Trip AS t JOIN HHSurvey.Trip AS nxt ON t.personid = nxt.personid AND t.tripnum + 1 = nxt.tripnum JOIN HHSurvey.ApiHomeTravel AS aht ON t.recid = aht.recid;
+		 	SET nxt.origin_purpose=1, nxt.origin_lat=ta.home_geog.Lat, nxt.origin_lng=ta.home_geog.Long
+			FROM HHSurvey.Trip AS t JOIN HHSurvey.Trip AS nxt ON t.personid = nxt.personid AND t.tripnum + 1 = nxt.tripnum JOIN tmpApi2Home AS ta ON t.recid = ta.new_recid;
+
+		EXECUTE HHSurvey.recalculate_after_edit;
+		EXECUTE HHSurvey.generate_error_flags;	
 		END
 		GO
 
