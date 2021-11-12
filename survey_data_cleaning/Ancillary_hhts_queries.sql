96d08157f5c8e2c79b00f2d9e71aa7dfb6eadcea
Michael Jensen <mjensen@psrc.org>
2021-11-12 13:57:35 -0800

    Shifted diagnostic queries between files

--- survey_data_cleaning/Ancillary_hhts_queries.sql
+++ survey_data_cleaning/Ancillary_hhts_queries.sql
@@ -1,49 +1,3 @@
---Error Flag reporting crosstab
-
-		WITH elevated AS 
-			(SELECT cte_t.personid FROM HHSurvey.Trip AS cte_t
-				WHERE (cte_t.psrc_comment IS NOT NULL) 
-				GROUP BY cte_t.personid)
-		SELECT error_flag, pivoted.[1] AS rMove, pivoted.[2] AS rSurvey, pivoted.[3] AS elevated
-		FROM (SELECT tef.error_flag, CASE WHEN e.personid IS NOT NULL THEN 3 ELSE t.hhgroup END AS category, count(t.recid) AS n
-				FROM HHSurvey.Trip AS t 
-					JOIN HHSurvey.trip_error_flags AS tef ON t.recid = tef.recid 
-					LEFT JOIN elevated AS e ON t.personid = e.personid 
-				WHERE t.psrc_resolved IS NULL
-				GROUP BY tef.error_flag, CASE WHEN e.personid IS NOT NULL THEN 3 ELSE t.hhgroup END) AS source
-		PIVOT (SUM(n) FOR category IN ([1], [2], [3])) AS pivoted
-		ORDER BY pivoted.[1] DESC;
-
-
-		SELECT error_flag, /*pivoted.[1] AS rMove,*/ pivoted.[2] AS rSurvey
-		FROM (SELECT tef.error_flag, t.data_source AS category, count(t.recid) AS n
-				FROM HHSurvey.Trip AS t 
-					JOIN HHSurvey.trip_error_flags AS tef ON t.recid = tef.recid 
-				WHERE t.psrc_resolved IS NULL
-				GROUP BY tef.error_flag, t.data_source) AS source
-		PIVOT (SUM(n) FOR category IN ([1], [2])) AS pivoted
-		ORDER BY pivoted.[1] DESC;
-
---Revision Code count
-
-			  SELECT 1, count(*) FROM HHSurvey.Trip AS t WHERE Elmer.dbo.rgx_find(t.revision_code,'\b1,',1)=1
-		UNION SELECT 2, count(*) FROM HHSurvey.Trip AS t WHERE Elmer.dbo.rgx_find(t.revision_code,'\b2,',1)=1
-		UNION SELECT 3, count(*) FROM HHSurvey.Trip AS t WHERE Elmer.dbo.rgx_find(t.revision_code,'\b3,',1)=1
-		UNION SELECT 4, count(*) FROM HHSurvey.Trip AS t WHERE Elmer.dbo.rgx_find(t.revision_code,'\b4,',1)=1
-		UNION SELECT 5, count(*) FROM HHSurvey.Trip AS t WHERE Elmer.dbo.rgx_find(t.revision_code,'\b5b?,',1)=1
-		UNION SELECT 6, count(*) FROM HHSurvey.Trip AS t WHERE Elmer.dbo.rgx_find(t.revision_code,'\b6,',1)=1
-		UNION SELECT 7, count(*) FROM HHSurvey.Trip AS t WHERE Elmer.dbo.rgx_find(t.revision_code,'\b7,',1)=1
-		UNION SELECT 8, count(*) FROM HHSurvey.Trip AS t WHERE Elmer.dbo.rgx_find(t.revision_code,'\b8,',1)=1
-		UNION SELECT 9, count(*) FROM HHSurvey.Trip AS t WHERE Elmer.dbo.rgx_find(t.revision_code,'\b9,',1)=1
-		UNION SELECT 10, count(*) FROM HHSurvey.Trip AS t WHERE Elmer.dbo.rgx_find(t.revision_code,'\b10,',1)=1
-		UNION SELECT 11, count(*) FROM HHSurvey.Trip AS t WHERE Elmer.dbo.rgx_find(t.revision_code,'\b11,',1)=1
-		UNION SELECT 12, count(*) FROM HHSurvey.Trip AS t WHERE Elmer.dbo.rgx_find(t.revision_code,'\b12,',1)=1
-		UNION SELECT 13, count(*) FROM HHSurvey.Trip AS t WHERE Elmer.dbo.rgx_find(t.revision_code,'\b13,',1)=1
-		UNION SELECT 14, count(*) FROM HHSurvey.Trip AS t WHERE Elmer.dbo.rgx_find(t.revision_code,'\b14,',1)=1
-		UNION SELECT 15, count(*) FROM HHSurvey.Trip AS t WHERE Elmer.dbo.rgx_find(t.revision_code,'15,',1)=1
-		UNION SELECT 16, count(*) FROM HHSurvey.Trip AS t WHERE Elmer.dbo.rgx_find(t.revision_code,'\b16,',1)=1
-		UNION SELECT 17, count(*) FROM HHSurvey.Trip AS t WHERE Elmer.dbo.rgx_find(t.revision_code,'\b17,',1)=1;
-
 --Generic value lookup--alter as necessary
 
 		SELECT cbv.[value], cbv.label
@@ -160,3 +114,49 @@ AND t.origin_lat BETWEEN 44 and 50 AND t.dest_lat BETWEEN 44 AND 50 AND recid BE
 UPDATE tu 
 	SET tu.trip_path_distance = CAST(LEFT(cte.mi_min_result, CHARINDEX(',', cte.mi_min_result)-1) AS float)
 	FROM HHSurvey.Trip AS tu JOIN cte ON tu.recid=cte.recid WHERE cte.mi_min_result<>'0,0';
+
+--Update geoassignments; for county first use rectangular approximation to save computation
+
+ALTER TABLE HHSurvey.Trip ADD dest_geom GEOMETRY NULL;
+
+ALTER TABLE HHSurvey.Trip ADD origin_geom GEOMETRY NULL;
+
+UPDATE t SET t.dest_geom=Elmer.dbo.ToXY(t.dest_lng, t.dest_lat) FROM HHSurvey.Trip AS t;
+
+UPDATE t SET t.origin_geom=Elmer.dbo.ToXY(t.origin_lng, t.origin_lat) FROM HHSurvey.Trip AS t;
+
+CREATE SPATIAL INDEX dest_geom_idx ON HHSurvey.Trip(dest_geom) USING GEOMETRY_AUTO_GRID
+  WITH (BOUNDING_BOX = (xmin = 1095800, ymin = -97600, xmax = 1622700, ymax = 477600));
+
+  CREATE SPATIAL INDEX origin_geom_idx ON HHSurvey.Trip(origin_geom) USING GEOMETRY_AUTO_GRID
+  WITH (BOUNDING_BOX = (xmin = 1095800, ymin = -97600, xmax = 1622700, ymax = 477600));
+
+ALTER TABLE HHSurvey.Trip ADD region_tripends int DEFAULT 0;
+
+UPDATE t SET t.region_tripends=t.region_tripends+1 
+FROM HHSurvey.Trip AS t JOIN ElmerGeo.dbo.PSRC_REGIONAL_OUTLINE AS r ON r.Shape.STIntersects(t.dest_geom)=1;
+
+UPDATE t SET t.region_tripends=t.region_tripends+1 
+FROM HHSurvey.Trip AS t JOIN ElmerGeo.dbo.PSRC_REGIONAL_OUTLINE AS r ON r.Shape.STIntersects(t.origin_geom)=1;
+
+UPDATE t SET t.dest_city=r.city_name 
+FROM HHSurvey.Trip AS t JOIN ElmerGeo.dbo.PSRC_REGION AS r ON r.Shape.STIntersects(t.dest_geom)=1
+WHERE r.feat_type='city';
+
+UPDATE t SET t.dest_zip=r.zipcode 
+FROM HHSurvey.Trip AS t JOIN ElmerGeo.dbo.ZIP_CODES AS r ON r.Shape.STIntersects(t.dest_geom)=1;
+
+SELECT t.region_tripends, count(*) FROM HHSurvey.Trip AS t GROUP BY t.region_tripends;
+
+UPDATE t
+SET t.dest_county=CASE WHEN (t.dest_lat BETWEEN 47.32417899933368 AND 47.77557543545566) AND (t.dest_lng BETWEEN -122.40491513697908 AND -121.47382388080176) THEN '033'
+					   WHEN (t.dest_lat BETWEEN 46.987025526142794 AND 47.25521385921765) AND (t.dest_lng BETWEEN -122.61999268125203 AND -122.14483401659517) THEN '053'
+					   WHEN (t.dest_lat BETWEEN 47.785624118154686 AND 48.29247321335945) AND (t.dest_lng BETWEEN -122.34422210698376 AND -121.18653784598449) THEN '061'
+					   WHEN (t.dest_lat BETWEEN 47.5126145395748 AND 47.7726115311967) AND (t.dest_lng BETWEEN -122.73894212405432 AND -122.50273608266419) THEN '035'
+					   ELSE NULL END
+FROM HHSurvey.Trip AS t WHERE t.dest_county IS NULL;
+
+UPDATE t
+SET t.dest_county = r.county_fip
+FROM HHSurvey.Trip AS t JOIN ElmerGeo.dbo.COUNTY_LINES AS r ON t.dest_geom.STIntersects(r.Shape)=1
+WHERE r.psrc=1;
