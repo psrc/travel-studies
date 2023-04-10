a68d9d2e34b70e91ece0ab98fdf0cb93f0527b2e
Suzanne Childress <childresssuzanne@gmail.com>
2023-04-10 16:04:28 -0700

    Update plot_modes.R

--- 2021/analysis_in_progress/rgcs/plot_modes.R
+++ 2021/analysis_in_progress/rgcs/plot_modes.R
@@ -6,4 +6,12 @@ rgc_mode<-read.csv('rgcs/rgc_mode_share.csv')
 
 rgc_mode_17_19<-rgc_mode%>%filter(survey=='2017/2019')%>%filter(mode!='Total')
 
-static_column_chart(rgc_mode_17_19, x='mode', y='share', fill='RGCs', moe='share_moe')
+static_column_chart(rgc_mode_17_19, x='mode', y='share', fill='RGCs', moe='share_moe',
+                     title='All Trip Mode Share by RGC Trip Destination Location', 
+                    subtitle='2017/2019 Household Travel Survey')
+
+rgc_mode_21<-rgc_mode%>%filter(survey=='2021')%>%filter(mode!='Total')
+
+static_column_chart(rgc_mode_21, x='mode', y='share', fill='RGCs', moe='share_moe',
+                    title='All Trip Mode Share by RGC Trip Destination Location',
+                    ,subtitle='2021 Household Travel Survey')
