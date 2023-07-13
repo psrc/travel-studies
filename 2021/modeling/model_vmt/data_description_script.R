962f96a52425a46ed5fc5692be571070350ef244
Joanne <71201494+Joanneylin@users.noreply.github.com>
2023-07-13 16:57:27 -0700

    weighted correlation matrix

--- 2021/modeling/model_vmt/data_description_script.R
+++ 2021/modeling/model_vmt/data_description_script.R
@@ -1,13 +1,85 @@
-library(echarts4r)
-library(psrcplot)
-library(psrc.travelsurvey)
-library(psrccensus)
-library(psrcelmer)
-library(tidyverse)
+# library(echarts4r)
+# library(psrcplot)
+# library(psrc.travelsurvey)
+# library(psrccensus)
+# library(psrcelmer)
+# library(tidyverse)
 
-install_psrc_fonts()
+# install_psrc_fonts()
+# 
+# Sys.setenv(CENSUS_API_KEY = '3fc20d0d6664692c0becc323b82c752408d843d9')
+# Sys.getenv("CENSUS_API_KEY")
+# 
+# hhts_codebook <- get_table(schema = 'HHSurvey', tbl_name = 'variables_codebook')
 
-Sys.setenv(CENSUS_API_KEY = '3fc20d0d6664692c0becc323b82c752408d843d9')
-Sys.getenv("CENSUS_API_KEY")
+# vmt data for analysis and modeling
+working_vars <- c("person_id","household_id","sample_segment","survey",
+                  "sample_county","final_home_rgcnum","urban_metro",
+                  "gender","age_category","race_eth_broad","have_child",
+                  "employment","license","hhincome_broad","vehicle_count",
+                  "transit_pass","work_parking","commute_mode_simple",
+                  "freq_transit","telecommute_freq",
+                  "hh_weight_2017_2019","hh_weight_2017_2019_adult")
+vmt_per <- trip_data_17_19 %>%
+  filter(mode_simple=="Drive") %>%
+  mutate(vmt= trip_path_distance/travelers_total) %>%
+  group_by(person_id) %>%
+  summarise(total_vmt=sum(vmt, na.rm = TRUE),
+            num_day = length(unique(daynum)),
+            n_trip_drive = sum(mode_simple=="Drive")) %>%
+  ungroup() %>%
+  mutate(vmt_day = total_vmt/num_day) %>%
+  full_join(per_data_17_19 %>% select(working_vars), by = "person_id") %>%
+  mutate(vmt_day=replace_na(vmt_day,0)) %>%
+  # other categories for statistics
+  filter(person_id!=19100243801) %>% # wrong distance for shopping trip
+  mutate(
+    vehicle_count_num = as.numeric(substr(vehicle_count,1,1)),
+    q_group = factor(ntile(vmt_day, 10)), # 10 guantile groups
+    no_vmt_grp = case_when(vmt_day==0~"zero vmt", # people with no vmt
+                           TRUE~"vmt"),
+    freq_transit = factor(freq_transit,
+                          levels=c("I never do this","I do this, but not in the past 30 days","1-3 times in the past 30 days",
+                                   "1 day/week","2-4 days/week","5 days/week","6-7 days/week")),
+    freq_transit_simple = factor(case_when(freq_transit %in% c("6-7 days/week","5 days/week")~"5+ days/week",
+                                           freq_transit %in% c("2-4 days/week", "1 day/week")~"1-4 days/week",
+                                           TRUE~freq_transit),
+                                 c("5+ days/week",
+                                   "1-4 days/week",
+                                   "1-3 times in the past 30 days",
+                                   "I do this, but not in the past 30 days",
+                                   "I never do this")),
+    telecommute_freq_simple = factor(case_when(telecommute_freq %in% c("Less than monthly","A few times per month")~"Less than weekly",
+                                               telecommute_freq %in% c("5 days a week","6-7 days a week")~"5+ days a week",
+                                               TRUE~telecommute_freq), 
+                                     levels=c("Never","Less than weekly",
+                                              "1 day a week","2 days a week","3 days a week","4 days a week",
+                                              "5+ days a week","Not applicable")))
 
-hhts_codebook <- get_table(schema = 'HHSurvey', tbl_name = 'variables_codebook')
+# data to test correlation
+cor_test <- vmt_per %>%
+  select(vmt_day,person_id,
+         hhincome_broad,telecommute_freq,freq_transit,transit_pass,work_parking,vehicle_count_num,
+         license,have_child,
+         hh_weight_2017_2019) %>%
+  filter(!is.na(vmt_day)) %>%
+  mutate(
+    # ordinal
+    hhincome_ord = as.numeric(case_when(
+      hhincome_broad %in% c("Under $25,000","$25,000-$49,999","$50,000-$74,999",
+                            "$75,000-$99,999","$100,000 or more")~hhincome_broad,
+      TRUE~NA)),
+    telecommute_freq_ord = as.numeric(factor(telecommute_freq, 
+                                             levels=c("Never","Less than monthly","A few times per month",
+                                                      "1 day a week","2 days a week","3 days a week","4 days a week",
+                                                      "5 days a week","6-7 days a week"))),
+    freq_transit_ord = as.numeric(freq_transit),
+    
+    # binary
+    transit_pass_binary = case_when( transit_pass == "Not offered"~0,
+                                     transit_pass == "Offered"~1,
+                                     TRUE~NA),
+    work_parking_binary = case_when( work_parking == "paid parking"~0,
+                                     work_parking == "free/reimbursed parking"~1,
+                                     TRUE~NA),
+    have_child = as.numeric(have_child)-1)
