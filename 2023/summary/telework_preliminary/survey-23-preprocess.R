53241e8dcccfb7df8588848aef50b2182677e1f0
Ennazus <childresssuzanne@gmail.com>
2024-04-18 09:34:49 -0700

    clean up

--- 2023/summary/telework_preliminary/survey-23-preprocess.R
+++ 2023/summary/telework_preliminary/survey-23-preprocess.R
@@ -1,60 +1,54 @@
 
 # get new labels associated with a group  
-get_grouped_labels<-function(.value_labels=value_labels,group_id, group_name){
+get_grouped_labels <- function(.value_labels = value_labels, group_id, group_name) {
+  group_title <- paste0(group_id, '_title')
+  group_value <- paste0(group_id, '_value')
   
-  group_title<-paste0(group_id, '_title')
-  group_value<-paste0(group_id, '_value')
+  cols <- c('label', group_value)
+  group_labels <- .value_labels[get(eval(group_title)) == group_name, ..cols]
   
-  group_labels   <- .value_labels%>%
-  filter(!!sym(group_title) == group_name)%>%
-  select('label', !!sym(group_value))
-
- 
-return(group_labels)
+  return(group_labels)
 }
 
 
 # function for grouping variable and adding it to the values list
-add_values_code<- function(.group_labels=group_labels, .value_labels=value_labels, group_name){
-  names_labels<-names(.group_labels)
-  group_id_value<-names_labels[grepl('group', names_labels)]
+add_values_code <- function(.group_labels = group_labels, .value_labels = value_labels, group_name) {
 
-  var_val_labels<- .group_labels%>%
-    mutate(variable=group_name) %>%
-    distinct(!!as.name(group_id_value), .keep_all=TRUE)%>%
-    rowid_to_column(var='value')%>%
-    select(variable, value, !!as.name(group_id_value))%>%
-    rename(label=!!as.name(group_id_value))%>%
-    mutate(group_1_title = NA, group_1_value = NA,
-           group_2_title = NA, group_2_value= NA,
-           group_3_title = NA, group_3_value = NA)
+  names_labels <- names(.group_labels)
+  group_id_value <- names_labels[grepl('group', names_labels)]
+
+  v <- unique(.group_labels, by = group_id_value)
+  v <- v[, value := .I][, .(variable = group_name, value, label = get(eval(group_id_value)))]
+
+  new_cols <- map( c('title', 'value'), ~paste('group', 1:3, .x, sep = '_')) |> unlist()
+  v[, (new_cols) := NA]
+
+  new_val_labels <- rbindlist(list(.value_labels, v), fill=TRUE)
+  new_val_labels[, val_order := .I]
   
-  all_value_labels<-.value_labels%>%select(variable, value, label, group_1_title, group_1_value,
-                                      group_2_title, group_2_value, group_3_title, group_3_value)
-  new_value_labels<-rbind(all_value_labels, var_val_labels)
-  new_value_labels[, val_order := seq_len(nrow(new_value_labels))]
-  new_value_labels<-setDT(new_value_labels)
-  
-  return(new_value_labels) 
+  return(new_val_labels) 
 }
 
-grp_to_tbl<-function(.group_labels=group_labels, tbl, ungrouped_name, grouped_name){
-
-  tbl <-left_join(tbl, .group_labels, by=setNames('label', ungrouped_name))
+grp_to_tbl <- function(.group_labels = group_labels, tbl, ungrouped_name, grouped_name) {
+  tbl <- left_join(tbl, .group_labels, by = setNames('label', ungrouped_name))
   #find what the group id is based on the group_labels
   # find the group text based on the group_labels
-  names_labels<-names(.group_labels)
-  group_id_value<-names_labels[grepl('group', names_labels)]
+  names_labels <- names(.group_labels)
+  group_id_value <- names_labels[grepl('group', names_labels)]
   
   setnames(tbl, group_id_value, grouped_name)
   return(tbl)
 }
 
+order_factors<-function(tbl, variable_name, value_labels){
+  var_val_labels<-value_labels%>%filter(variable==variable_name)
+  tbl<-tbl%>%left_join(var_val_labels, by=join_by(!!sym(variable_name)==label))%>%
+    arrange(val_order)%>%
+    mutate({{variable_name}}:=factor(!!sym(variable_name)))
+  return(tbl)
+}
 
-
-
-
-summarize_weighted<-function(hts_data, summarize_var, summarize_by, id_cols, wt_cols,wtname){
+summarize_weighted <- function(hts_data, summarize_var, summarize_by, id_cols, wt_cols,wtname){
   
   
   prepped_dt <- hts_prep_variable(summarize_var = summarize_var,
