ef69272d3777628add70e13b4ccfd1b07129bfcb
cyyllam <cyyllam@gmail.com>
2024-04-30 15:19:26 -0700

    loop thru geogs

--- 2023/summary/shiny/translate-pseudocode.R
+++ 2023/summary/shiny/translate-pseudocode.R
@@ -49,10 +49,15 @@ names(dfs)[[1]] <- "hh"
 ids <- c('household_id', 'person_id', 'day_id', 'trip_id')
 wts <- c('hh_weight', 'person_weight', 'day_weight', 'trip_weight')
 
-test_variables <- c("age", "transit_pass", "broadband")
+# vars <- c("age", "transit_pass", "broadband")
+exc_vars <- c('survey_year', 'person_id', 'tripid')
+vars <- unique(variable_list$variable)[!(variable_list$variable %in% exc_vars)]
+
+geogs <- c('Region', 'Seattle', "Bellevue")
 trends_df <- NULL
 
-for(var in test_variables) {
+for(var in vars) {
+  for(geog in geogs) {
   source_table <- variable_list[variable == var, .(variable, hh, person, day, trip)] |>
     melt.data.table(id.vars = 'variable',
                     measure.vars = c('hh', 'person', 'day', 'trip'),
@@ -62,16 +67,24 @@ for(var in test_variables) {
     as.character() |> 
     paste0("_weight")
   
-  summarize_df <- summarize_weighted(hts_data = dfs,
+  if(geog != 'Region') {
+    households <- dfs$hh[home_jurisdiction == geog]
+  } else {
+    households <- dfs$hh
+  }
+  
+  summarize_df <- summarize_weighted(hts_data = list(hh = households, person = dfs$person, day = dfs$day, trip = dfs$trip),
                                      summarize_var = var,
                                      summarize_by = 'survey_year',
                                      id_cols = ids,
                                      wt_cols = wts,
                                      wtname = weight_name) # weight name corresponds to table variable is from
   
-  df <- summarize_df$summary$wtd[, variable_name := var]
+  df <- summarize_df$summary$wtd[, `:=` (variable_name = var, geography = geog)]
+  
   setnames(df, var, "value")
   ifelse(is.null(trends_df), trends_df <- df, trends_df <- rbindlist(list(trends_df, df), fill=TRUE))
+  }
 }
 
 
