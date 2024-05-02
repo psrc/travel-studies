c80d6ffe9f6adebb9f140814a346bf8ca0774912
cyyllam <cyyllam@gmail.com>
2024-05-01 17:17:40 -0700

    initiate crosstab loop

--- 2023/summary/shiny/translate-pseudocode.R
+++ 2023/summary/shiny/translate-pseudocode.R
@@ -7,6 +7,8 @@ library(openxlsx)
 
 source(here::here('2023/summary/survey-23-preprocess.R'))
 
+current_year <- 2023
+
 ## Read in Codebook
 cb_path <- here::here("2023/summary/shiny/PSRC_Codebook_2023_for_shiny.xlsx")
 
@@ -31,7 +33,7 @@ names(dfs) <- dataset_types
 change_cols <- c(paste(dataset_types, 'id', sep = "_"), 'survey_year')
 
 for(i in 1:length(dfs)) {
-
+  
   if(names(dfs[i]) == 'household') {
     cols <- change_cols[c(1, 5)]
   } else if(names(dfs[i]) == 'person') {
@@ -54,6 +56,8 @@ exc_vars <- c('survey_year', 'person_id', 'tripid')
 vars <- unique(variable_list$variable)[!(variable_list$variable %in% exc_vars)]
 
 geogs <- c('Region', 'Seattle', "Bellevue")
+
+tab 1
 trends_df <- NULL
 
 for(var in vars) {
@@ -62,29 +66,84 @@ for(var in vars) {
     melt.data.table(id.vars = 'variable',
                     measure.vars = c('hh', 'person', 'day', 'trip'),
                     variable.name = 'table')
-  
-  weight_name <- source_table[value == 1,][['table']] |> 
-    as.character() |> 
+
+  weight_name <- source_table[value == 1,][['table']] |>
+    as.character() |>
     paste0("_weight")
-  
+
   if(geog != 'Region') {
     households <- dfs$hh[home_jurisdiction == geog]
   } else {
     households <- dfs$hh
   }
-  
+
   summarize_df <- summarize_weighted(hts_data = list(hh = households, person = dfs$person, day = dfs$day, trip = dfs$trip),
                                      summarize_var = var,
                                      summarize_by = 'survey_year',
                                      id_cols = ids,
                                      wt_cols = wts,
                                      wtname = weight_name) # weight name corresponds to table variable is from
-  
+
   df <- summarize_df$summary$wtd[, `:=` (variable_name = var, geography = geog)]
-  
+
   setnames(df, var, "value")
   ifelse(is.null(trends_df), trends_df <- df, trends_df <- rbindlist(list(trends_df, df), fill=TRUE))
   }
 }
+
+# tab 2
+# matrix of every single variable combo...
+n <- 2
+vars <- c("age", "transit_pass", "broadband")
+pairings <- lapply(numeric(n), function(x) vars)
+combos <- as.data.table(expand.grid(pairings))
+combos <- combos[Var1 != Var2, ]
+
+crosstab_df <- NULL
+
+for(i in 1:nrow(combos)) {
+  for(geog in geogs) {
+    
+    v1 <- as.character(combos[i,][['Var1']])
+    v2 <- as.character(combos[i,][['Var2']])
+    
+    source_table <- variable_list[variable %in% c(v1, v2), .(variable, hh, person, day, trip)] |>
+      melt.data.table(id.vars = 'variable',
+                      measure.vars = c('hh', 'person', 'day', 'trip'),
+                      variable.name = 'table')
+    
+    weight_name <- source_table[value == 1,][['table']] |> unique() |> as.character()
+    if(length(weight_name) == 1) {
+      weight_name <- paste0(weight_name, "_weight")
+    }
+    
+    # evaluate weight hierarchy
+    
+
+    # filter dfs tables for current year (2023)
+    dfs_cyr <- copy(dfs)
+    dfs_cyr <- map(dfs_cyr, ~.x[survey_year == current_year, ])     
+    
+    if(geog != 'Region') {
+      households <- dfs_cyr$hh[home_jurisdiction == geog]
+    } else {
+      households <- dfs_cyr$hh
+    }
+    
+    summarize_df <- summarize_weighted(hts_data = list(hh = households, person = dfs_cyr$person, day = dfs_cyr$day, trip = dfs_cyr$trip),
+                                       summarize_var = v1,
+                                       summarize_by = v2,
+                                       id_cols = ids,
+                                       wt_cols = wts,
+                                       wtname = weight_name) # weight name corresponds to table variable is from
+    
+    df <- summarize_df$summary$wtd[, `:=` (var1 = v1, var2 = v2, geography = geog)]
+    
+    setnames(df, c(v1, v2), c('val1', 'val2'))
+    ifelse(is.null(crosstab_df), crosstab_df <- df, crosstab_df <- rbindlist(list(crosstab_df, df), fill=TRUE))
+    
+  }
+}
+
 
 
