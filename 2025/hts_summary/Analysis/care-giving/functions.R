feabe4cb85166d194a9bb28357a92abefa6d120e
cyyllam <cyyllam@gmail.com>
2026-04-14 16:21:55 -0700

    creating quarto book for care giving

--- 2025/hts_summary/Analysis/care-giving/functions.R
+++ 2025/hts_summary/Analysis/care-giving/functions.R
@@ -1,13 +1,14 @@
 create_trip_purpose_tbl <- function(hts_data) {
   # This section prints a table with weighted/unweighted x Home/Exclude Home x trip purpose
-
-  rs1 <- psrc_hts_stat(hts_data,
+  # region
+  
+  rs <- psrc_hts_stat(hts_data,
                        analysis_unit = "trip",
                        group_vars = c("dest_region","dest_purpose_cat"),
                        incl_na = FALSE) |>
     mutate(type = "Include HOME")
   
-  rs1_no_home <- psrc_hts_stat(hts_data,
+  rs_no_home <- psrc_hts_stat(hts_data,
                                analysis_unit = "trip",
                                group_vars = c("dest_region","dest_purpose_cat_no_home"),
                                incl_na = FALSE)  |>
@@ -16,19 +17,66 @@ create_trip_purpose_tbl <- function(hts_data) {
   
   # Region
   
-  rs <- rs1 |>  
-    add_row(rs1_no_home)
+  rs <- rs |>  
+    add_row(rs_no_home)
   
-  rs_reg <- rs |>
+  rs_calc <- rs |>
     mutate(unweighted = percent(count/sum(count)),
            weighted = percent(prop),
-           .by = "type")
-  
-  per_group <- ("dest_purpose_cat")
+           .by = c(type, survey_year))
     
-  df <- rs_reg |>
-    pivot_wider(id_cols = all_of(per_group),
+  df <- rs_calc |>
+    pivot_wider(id_cols = "dest_purpose_cat",
                 names_from = c(type, survey_year),
                 values_from = contains("weighted"),
+                names_sep = ".") 
+}
+
+create_care_purpose_tbl <- function(hts_data) {
+  # region
+  
+  rs <- psrc_hts_stat(df_hts,
+                      analysis_unit = "trip",
+                      group_vars = c("dest_region", "care_purpose_cat"),
+                      incl_na = FALSE)
+
+  df_calc <- rs |> 
+    mutate(unweighted = percent(count/sum(count)),
+           weighted = percent(prop),
+           moe = percent(prop_moe),
+           .by = survey_year)
+  
+  df <- df_calc |>
+    pivot_wider(id_cols = "care_purpose_cat",
+                names_from = survey_year,
+                values_from = contains("weighted"),
+                names_sep = ".") 
+  
+}
+
+mode_share_income <- function(income, geog){
+  geo_col <- case_when(geog == "Region" ~ "dest_region",
+                       geog %in% c("King County", "Kitsap County", "Pierce County", "Snohomish County") ~ "dest_county")
+
+  rs <- psrc_hts_stat(df_hts,
+                       analysis_unit = "trip",
+                       group_vars = c(geo_col, "care_purpose_cat", income, "mode_class_5"),
+                       incl_na = FALSE) |>
+    rename(dest_loc = sym(geo_col)) |>
+    filter(dest_loc == geog,
+           care_purpose_cat == "Care") |>
+    mutate(type = "percent") |>
+    mutate(unweighted = percent(count/sum(count)),
+           weighted = percent(prop),
+           moe = percent(prop_moe),
+           .by = c("survey_year", "type", income))
+
+  v <- c("survey_year", "dest_loc", "care_purpose_cat", income, "mode_class_5", "unweighted", "weighted", "moe")
+
+  rs |> 
+    select(all_of(v)) |> 
+    pivot_wider(id_cols = c("dest_loc", "care_purpose_cat", , "mode_class_5"),
+                names_from = c("survey_year", contains("income")),
+                values_from = c(contains("weighted"), "moe"),
                 names_sep = ".") 
 }
