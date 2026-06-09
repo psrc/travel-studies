889de7f60178470fa46485f342864c2dd521d2e5
Joanne <71201494+Joanneylin@users.noreply.github.com>
2026-06-09 09:59:25 -0700

    update summary document

--- 2025/hts_summary/Analysis/util.R
+++ 2025/hts_summary/Analysis/util.R
@@ -8,29 +8,25 @@ library(psrccensus)
 library(psrcplot)
 install_psrc_fonts()
 
-log_add_group_var <- function(var_name, table){
-  
-  log_info(glue("{table} -new variable `{var_name}` added: ", 
-                paste0(levels(df_hts_analysis[[table]][[var_name]]), collapse = " > ")))
-  
-}
 
-get_percent_format <- function(.data){
+get_table_format <- function(.data){
   .data  %>%
-    mutate(prop_pct = percent_format(prop), # prop for printed tables
-           prop_moe_pct = percent_format(prop_moe,0.001), # prop_moe for printed tables
-           prop_label = percent_format(prop,1)) # rounded for chart labels
+    mutate(prop = scales::percent(prop, accuracy = 0.1),
+           prop_moe = scales::percent(prop_moe, accuracy = 0.01),
+           est = scales::number(est, accuracy = 1, big.mark = ","),
+           count = scales::number(count, accuracy = 1, big.mark = ",")) %>%
+    select(-est_moe)
 }
 
-plot_single_var <- function(df_plot, var_name, limits=NULL){
-  ggplot(df_plot, aes(x={{var_name}}, y=prop, fill=factor(survey_year))) +
-    geom_col(position = position_dodge(),width = 0.8) +
-    geom_text(aes(label=prop_label),
-              vjust = -0.5,
-              position = position_dodge(0.8)) +
-    scale_fill_manual(values = psrc_colors$pgnobgy_5)+
-    scale_y_continuous(labels = scales::percent, limits = limits) +
-    psrc_style() +
-    theme(panel.grid.major.y = element_blank(),
-          axis.title = element_blank())
-}
+# plot_single_var <- function(df_plot, var_name, limits=NULL){
+#   ggplot(df_plot, aes(x={{var_name}}, y=prop, fill=factor(survey_year))) +
+#     geom_col(position = position_dodge(),width = 0.8) +
+#     geom_text(aes(label=prop_label),
+#               vjust = -0.5,
+#               position = position_dodge(0.8)) +
+#     scale_fill_manual(values = psrc_colors$pgnobgy_5)+
+#     scale_y_continuous(labels = scales::percent, limits = limits) +
+#     psrc_style() +
+#     theme(panel.grid.major.y = element_blank(),
+#           axis.title = element_blank())
+# }
